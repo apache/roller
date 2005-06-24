de22a5418095c980d0524c6826e34c15ad6b098d
David M. Johnson <snoopdave@apache.org>
2005-06-24 04:51:40 +0000

    still cleaning up after breaking website/user relationship
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/branches/roller_2.0@201565 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/roller/pojos/WebsiteObject.java
@@ -0,0 +1,29 @@
+package org.roller.pojos;
+
+import org.roller.RollerException;
+import org.roller.model.Roller;
+import org.roller.model.RollerFactory;
+
+/** 
+ * Base class for any object that belongs to a website.
+ */
+public abstract class WebsiteObject extends PersistentObject
+{
+    public abstract WebsiteData getWebsite();
+    
+    public boolean canSave() throws RollerException
+    {
+        Roller roller = RollerFactory.getRoller();
+        if (roller.getUser().equals(UserData.SYSTEM_USER)) 
+        {
+            return true;
+        }
+        if (getWebsite().hasUserPermissions(
+           roller.getUser(), (short)(PermissionsData.ADMIN|PermissionsData.AUTHOR)))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
