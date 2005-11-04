2c28814b5ecb6209df49312f234be99063722fc5
David M. Johnson <snoopdave@apache.org>
2005-11-04 18:14:08 +0000

    Better way to handle removal of not null constraint
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@330858 13f79535-47bb-0310-9956-ffa450edef68

--- metadata/database/120-to-130-migration-raw.sql
+++ metadata/database/120-to-130-migration-raw.sql
@@ -1,10 +1,2 @@
 
--- Upgrading from 1.2 to 1.3 requires one small change. The website table
--- column defaultpageid is now allowed to be null. Unfortunately, MySQL
--- requires non-standard alter table syntax for this.
-
--- So if you're on MySQL do this:
-alter table website modify defaultpageid varchar(48);
-
--- And for all other databases do this:
-alter table website alter column defaultpageid drop not null;
+@ALTER_DEFAULTPAGEID_ALLOW_NULL@;
