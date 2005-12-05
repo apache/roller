70f9744f9c34fd9d9b98fdd742d5b6d2b0f2a3fc
David M. Johnson <snoopdave@apache.org>
2005-12-05 03:11:54 +0000

    Fix for ROL-910, needs to be backported to roller_2.0 branch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@353964 13f79535-47bb-0310-9956-ffa450edef68

--- metadata/database/200-to-210-migration-raw.sql
+++ metadata/database/200-to-210-migration-raw.sql
@@ -1,10 +1,12 @@
 
 -- Add to roller_comment table: approved and pending fields
 
-alter table roller_comment add column approved @BOOLEAN_SQL_TYPE_FALSE@;
+alter table roller_comment add column approved @BOOLEAN_SQL_TYPE@;
+@ALTER_TABLE_ROLLER_COMMENT_APPROVED_DEFAULT_FALSE@;
 @ALTER_TABLE_ROLLER_COMMENT_APPROVED_NOT_NULL@;
 
-alter table roller_comment add column pending @BOOLEAN_SQL_TYPE_TRUE@;
+alter table roller_comment add column pending @BOOLEAN_SQL_TYPE@;
+@ALTER_TABLE_ROLLER_COMMENT_PENDING_DEFAULT_FALSE@;
 @ALTER_TABLE_ROLLER_COMMENT_PENDING_NOT_NULL@;
 
 update roller_comment set approved=1, pending=0, posttime=posttime;
@@ -12,13 +14,16 @@ update roller_comment set approved=1, pending=0, posttime=posttime;
 
 -- Add to website table: commentmod, blacklist, defaultallowcomments and defaultcommentdays 
 
-alter table website add column commentmod @BOOLEAN_SQL_TYPE_FALSE@;
+alter table website add column commentmod @BOOLEAN_SQL_TYPE@;
+@ALTER_TABLE_WEBSITE_COMMENTMOD_DEFAULT_FALSE@;
 @ALTER_TABLE_WEBSITE_COMMENTMOD_NOT_NULL@;
 
-alter table website add column defaultallowcomments @BOOLEAN_SQL_TYPE_TRUE@;
+alter table website add column defaultallowcomments @BOOLEAN_SQL_TYPE@;
+@ALTER_TABLE_WEBSITE_DEFAULTALLOWCOMMENTS_DEFAULT_TRUE@;
 @ALTER_TABLE_WEBSITE_DEFAULTALLOWCOMMENTS_NOT_NULL@;
 
 alter table website add column defaultcommentdays integer;
+@ALTER_TABLE_WEBSITE_DEFAULTCOMMENTDAYS_DEFAULT_7@;
 @ALTER_TABLE_WEBSITE_DEFAULTCOMMENTDAYS_NOT_NULL@;
 
 alter table website add column blacklist @TEXT_SQL_TYPE@;
