e1554f19c530cc1893fcdfc8e9a9fd9eb83d05ed
Anil R. Gangolli <gangolli@apache.org>
2005-12-18 17:47:16 +0000

    For ROL-934.  Support robot user-agent check in RefererFilter.  The property to configure this has been added to roller.properties with the setting commented out.  To enable, uncomment it and edit the pattern if/as desired.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@357482 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/roller/presentation/filters/RefererFilter.java
+++ src/org/roller/presentation/filters/RefererFilter.java
@@ -1,6 +1,8 @@
 package org.roller.presentation.filters;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -17,95 +19,107 @@ import org.roller.model.RefererManager;
 import org.roller.model.RollerFactory;
 import org.roller.presentation.RollerContext;
 import org.roller.presentation.RollerRequest;
+import org.roller.config.RollerConfig;
 
 /**
  * Keep track of referers.
  *
- * @web.filter name="RefererFilter"
- *
  * @author David M. Johnson
+ * @web.filter name="RefererFilter"
  */
 public class RefererFilter implements Filter {
-    private FilterConfig   mFilterConfig = null;
-    private static Log mLogger =
-            LogFactory.getFactory().getInstance(RefererFilter.class);
-    
+    private FilterConfig mFilterConfig = null;
+    private static Log mLogger = LogFactory.getFactory().getInstance(RefererFilter.class);
+    private static Pattern robotPattern = null;
+    private static final String ROBOT_PATTERN_PROP_NAME = "referrer.robotCheck.userAgentPattern";
+
     /**
      * destroy
      */
     public void destroy() {
     }
-    
+
     /**
      * doFilter
      */
-    public void doFilter(
-            ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest)req;
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         boolean isRefSpammer = false;
+        boolean isRobot = false;
+
         try {
-            RollerRequest rreq = RollerRequest.getRollerRequest(request);
-            RollerContext rctx = RollerContext.getRollerContext(
-                    mFilterConfig.getServletContext());
-            
-            if (rreq!=null && rreq.getWebsite() != null) {
-                String handle = rreq.getWebsite().getHandle();
-                
-                // Base page URLs, with and without www.
-                String basePageUrlWWW =
-                        rctx.getAbsoluteContextUrl(request)+"/page/"+handle;
-                String basePageUrl = basePageUrlWWW;
-                if ( basePageUrlWWW.startsWith("http://www.") ) {
-                    // chop off the http://www.
-                    basePageUrl = "http://"+basePageUrlWWW.substring(11);
-                }
-                
-                // Base comment URLs, with and without www.
-                String baseCommentsUrlWWW =
-                        rctx.getAbsoluteContextUrl(request)+"/comments/"+handle;
-                String baseCommentsUrl = baseCommentsUrlWWW;
-                if ( baseCommentsUrlWWW.startsWith("http://www.") ) {
-                    // chop off the http://www.
-                    baseCommentsUrl= "http://"+baseCommentsUrlWWW.substring(11);
-                }
-                
-                // Don't process hits from same user's blogs as referers by
-                // ignoring Don't process referer from pages that start with base URLs.
-                String referer = request.getHeader("Referer");
-                if (  referer==null ||
-                        (
-                        !referer.startsWith( basePageUrl )
-                        && !referer.startsWith( basePageUrlWWW )
-                        && !referer.startsWith( baseCommentsUrl )
-                        && !referer.startsWith( baseCommentsUrlWWW )
-                        )
-                        ) {
-                    RefererManager refMgr =
-                            RollerFactory.getRoller().getRefererManager();
-                    isRefSpammer = refMgr.processRequest(rreq);
-                } else {
-                    if (mLogger.isDebugEnabled()) {
-                        mLogger.debug("Ignoring referer="+referer);
+            if (robotPattern != null) {
+                // If the pattern is present, we check for whether the User-Agent matches,
+                // and set isRobot if so.  Currently, all referral processing, including
+                // spam check, is skipped for robots identified in this way.
+                String userAgent = request.getHeader("User-Agent");
+                isRobot = (userAgent != null && userAgent.length() > 0 && robotPattern.matcher(userAgent).matches());
+            }
+
+            if (!isRobot) {
+                RollerRequest rreq = RollerRequest.getRollerRequest(request);
+                RollerContext rctx = RollerContext.getRollerContext(mFilterConfig.getServletContext());
+
+                if (rreq != null && rreq.getWebsite() != null) {
+                    String handle = rreq.getWebsite().getHandle();
+
+                    // Base page URLs, with and without www.
+                    String basePageUrlWWW = rctx.getAbsoluteContextUrl(request) + "/page/" + handle;
+                    String basePageUrl = basePageUrlWWW;
+                    if (basePageUrlWWW.startsWith("http://www.")) {
+                        // chop off the http://www.
+                        basePageUrl = "http://" + basePageUrlWWW.substring(11);
+                    }
+
+                    // Base comment URLs, with and without www.
+                    String baseCommentsUrlWWW = rctx.getAbsoluteContextUrl(request) + "/comments/" + handle;
+                    String baseCommentsUrl = baseCommentsUrlWWW;
+                    if (baseCommentsUrlWWW.startsWith("http://www.")) {
+                        // chop off the http://www.
+                        baseCommentsUrl = "http://" + baseCommentsUrlWWW.substring(11);
+                    }
+
+                    // Don't process hits from same user's blogs as referers by
+                    // ignoring Don't process referer from pages that start with base URLs.
+                    String referer = request.getHeader("Referer");
+                    if (referer == null || (!referer.startsWith(basePageUrl) && !referer.startsWith(basePageUrlWWW) && !referer.startsWith(baseCommentsUrl) && !referer.startsWith(baseCommentsUrlWWW)))
+                    {
+                        RefererManager refMgr = RollerFactory.getRoller().getRefererManager();
+                        isRefSpammer = refMgr.processRequest(rreq);
+                    } else {
+                        if (mLogger.isDebugEnabled()) {
+                            mLogger.debug("Ignoring referer=" + referer);
+                        }
                     }
                 }
             }
         } catch (Exception e) {
-            mLogger.error("Processing referer",e);
+            mLogger.error("Processing referer", e);
         }
-        
+
         if (isRefSpammer) {
-            HttpServletResponse response = (HttpServletResponse)res;
+            HttpServletResponse response = (HttpServletResponse) res;
             response.sendError(HttpServletResponse.SC_FORBIDDEN);
         } else {
             chain.doFilter(req, res);
         }
     }
-    
+
     /**
      * init
      */
     public void init(FilterConfig filterConfig) throws ServletException {
         mFilterConfig = filterConfig;
+        String robotPatternStr = RollerConfig.getProperty(ROBOT_PATTERN_PROP_NAME);
+        if (robotPatternStr != null && robotPatternStr.length() >0) {
+            // Parse the pattern, and store the compiled form.
+            try {
+                robotPattern = Pattern.compile(robotPatternStr);
+            } catch (Exception e) {
+                // Most likely a PatternSyntaxException; log and continue as if it is not set.
+                mLogger.error("Error parsing "+ ROBOT_PATTERN_PROP_NAME + " value '" +
+                        robotPatternStr + "'.  Robots will not be filtered. ", e);
+            }
+        }
     }
 }
