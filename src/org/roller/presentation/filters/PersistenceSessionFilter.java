ec2e65244a395c09c6f8b530afb96e7d9df046b7
Allen T. Gilliland IV <agilliland@apache.org>
2006-01-14 01:17:22 +0000

    code reformatting.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/trunk@368943 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/roller/presentation/filters/PersistenceSessionFilter.java
+++ src/org/roller/presentation/filters/PersistenceSessionFilter.java
@@ -1,7 +1,6 @@
 package org.roller.presentation.filters;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -10,63 +9,49 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.roller.model.Roller;
 import org.roller.presentation.RollerContext;
 
+
 /**
- * Sole responsibility is to ensure that each request's Roller 
+ * Sole responsibility is to ensure that each request's Roller
  * persistence session is released at end of the request.
+ *
  * @web.filter name="PersistenceSessionFilter"
  * @author David M. Johnson
  */
-public class PersistenceSessionFilter implements Filter
-{
-    private static Log mLogger =
-        LogFactory.getFactory().getInstance(RequestFilter.class);
-
-    /**
-     * destroy
-     */
-    public void destroy()
-    {
-    }
-
+public class PersistenceSessionFilter implements Filter {
+    
+    private static Log mLogger = LogFactory.getLog(PersistenceSessionFilter.class);
+    
+    
     /**
      * Release Roller persistence session at end of request processing.
      */
-    public void doFilter(
-        ServletRequest req, ServletResponse res, FilterChain chain)
-        throws IOException, ServletException
-    {
-        HttpServletRequest request = (HttpServletRequest)req;
-        HttpServletResponse response = (HttpServletResponse)res;
-        if (mLogger.isDebugEnabled())
-        {
-            mLogger.debug("Entered PersistenceSessionFilter");
-        }
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        
+        mLogger.debug("Entered PersistenceSessionFilter");
+        
         Roller roller = RollerContext.getRoller((HttpServletRequest)request);
-        try 
-        {
+        try {
             chain.doFilter(request, response);
-        }
-        finally 
-        {
+        } finally {
             roller.release();
         }
-        if (mLogger.isDebugEnabled())
-        {
-            mLogger.debug("Exiting PersistenceSessionFilter");
-        }
+        
+        mLogger.debug("Exiting PersistenceSessionFilter");
     }
+    
 
-    /**
-     * init
-     */
-    public void init(FilterConfig filterConfig) throws ServletException
-    {
-    }
+    public void init(FilterConfig filterConfig) throws ServletException {}
+    
+    public void destroy() {}
+    
 }
 
