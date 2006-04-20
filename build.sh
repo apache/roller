70f6235f15b9f564283d807eff0d82a19fea8951
Elias Torres <eliast@apache.org>
2006-04-20 13:45:45 +0000

    Added support for cygwin by setting the appropriate path separator.
    --from an email by stefan.eissing@greenbytes.de to roller-dev. Thanks!
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@395589 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false
+os400=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+OS400*) os400=true;;
+esac
+
+CPSEP=":"
+if $cygwin; then CPSEP=";"; fi
+
 if [ "$JAVA_HOME" = "" ] ; then
 	echo You must set JAVA_HOME to point to your Java JDK install directory
 	exit 1
 fi
 
 rocp=${JAVA_HOME}/lib/tools.jar
-rocp=${rocp}:./tools/buildtime/ant-1.6.2/ant.jar
-rocp=${rocp}:./tools/buildtime/ant-1.6.2/ant-launcher.jar
-rocp=${rocp}:./tools/buildtime/ant-1.6.2/ant-junit.jar
-rocp=${rocp}:./tools/buildtime/ant-1.6.2/ant-commons-net.jar
-rocp=${rocp}:./tools/buildtime/ant-1.6.2/commons-net-1.2.2.jar
-rocp=${rocp}:./tools/buildtime/ant-1.6.2/jakarta-oro-2.0.8.jar
-rocp=${rocp}:./tools/buildtime/junit-3.8.1.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/ant-1.6.2/ant.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/ant-1.6.2/ant-launcher.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/ant-1.6.2/ant-junit.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/ant-1.6.2/ant-commons-net.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/ant-1.6.2/commons-net-1.2.2.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/ant-1.6.2/jakarta-oro-2.0.8.jar
+rocp=${rocp}${CPSEP}./tools/buildtime/junit-3.8.1.jar
+
+echo $rocp
 
 ${JAVA_HOME}/bin/java -Xmx512m -Djava.home=${JAVA_HOME} -classpath ${rocp} org.apache.tools.ant.Main $1 $2 $3 $4 $5
 
