9d8c9ea896fed86d3621e2aa4dc5dc69d54c0ce6
David M. Johnson <snoopdave@apache.org>
2006-05-01 19:45:43 +0000

    Adding Apache headers
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@398669 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/standalone/jspwiki/templates/roller2/search_highlight.js
+++ sandbox/standalone/jspwiki/templates/roller2/search_highlight.js
@@ -1,3 +1,20 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+*  contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
 /* http://www.kryogenix.org/code/browser/searchhi/ */
 /* Modified 20021006 to fix query string parsing and add case insensitivity */
 /* Modified 20030227 by sgala@hisitech.com to skip words with "-" and cut %2B (+) preceding pages */
--- sandbox/standalone/tomcat/bin/setenv.bat
+++ sandbox/standalone/tomcat/bin/setenv.bat
@@ -1,2 +1,19 @@
+@echo off
+rem Licensed to the Apache Software Foundation (ASF) under one or more
+rem  contributor license agreements.  The ASF licenses this file to You
+rem under the Apache License, Version 2.0 (the "License"); you may not
+rem use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.  For additional information regarding
+rem copyright in this work, please see the NOTICE file in the top level
+rem directory of this distribution.
+
 set JAVA_OPTS=-Dhsqldb_database=%CATALINA_HOME%\blogdata\rollerdb -Dhsqldb_port=7475 -DrollerPageDir=%CATALINA_HOME%\wikidata -DrollerStorageDir=%CATALINA_HOME%\wikiattachments
 set CATALINA_TMPDIR=\temp
