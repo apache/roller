daafe8aeb5673f535fa12012b45ae6cddb21232b
David M. Johnson <snoopdave@apache.org>
2006-05-01 19:44:21 +0000

    Adding Apache headers
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/trunk@398667 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/planetroller/planet-tool.bat
+++ sandbox/planetroller/planet-tool.bat
@@ -1,4 +1,21 @@
 @echo off
+rem Licensed to the Apache Software Foundation (ASF) under one or more
+rem  contributor license agreements.  The ASF licenses this file to You
+rem under the Apache License, Version 2.0 (the "License"); you may not
+rem use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.  For additional information regarding
+rem copyright in this work, please see the NOTICE file in the top level
+rem directory of this distribution.
+
+@echo off
 call setenv.bat
 set _CP=.;%PLANETCLASSES%
 set _CP=%_CP%;%FETCHERJARS%\commons-httpclient-2.0.2.jar
