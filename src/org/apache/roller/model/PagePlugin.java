5091c1e7788e6e3e74898b97e1e2ab1dfbb0fb6d
Allen T. Gilliland IV <agilliland@apache.org>
2006-06-15 21:30:53 +0000

    code reformatting.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/branches/roller_3.0@414687 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/model/PagePlugin.java
+++ src/org/apache/roller/model/PagePlugin.java
@@ -1,23 +1,21 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
-/*
- * Created on May 26, 2003
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
  */
+
 package org.apache.roller.model;
 
 import org.apache.velocity.context.Context;
@@ -25,21 +23,22 @@ import org.apache.roller.RollerException;
 import org.apache.roller.pojos.WeblogEntryData;
 import org.apache.roller.pojos.WebsiteData;
 
+
 /**
  * Interface for Roller weblog entry page plugins, which can transform
  * entry summary or text fields.
- *
- * @author David M Johnson
  */
 public interface PagePlugin {
+    
     public String name = "PagePlugin";
+    
     
     /**
      * Plugins can this as an opportunity to add any required objects
      * to the RollerRequest and the VelocityContext.
      * @param website     Website being processed
      * @param config      javax.servlet.ServletContext (or null if running outside webapp)
-     * @param baseURL     Base URL of Roller site 
+     * @param baseURL     Base URL of Roller site
      * @param ctx         Plugins may place objects into the Velocity Context.
      */
     public void init(
@@ -88,4 +87,5 @@ public interface PagePlugin {
     
     /** Returns true if this plugin should be skipped on single entry pages. */
     public boolean getSkipOnSingleEntry();
+    
 }
