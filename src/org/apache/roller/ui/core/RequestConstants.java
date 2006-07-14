1f646644a76850bd739ad78d9f4350e491cb8fdf
David M. Johnson <snoopdave@apache.org>
2006-07-14 16:48:01 +0000

    - Created RequestConstants class, moved essential constants from RollerRequest   to this new class.
    - Added "find current weblog logic" to BaseRollerMenu, but we really need to 
      find a more simple and consistent way to convey weblog state across all 
      requests.
    
    - WeblogCalendarModel no longer uses RollerRequest.
    
    - JSPs no longer use RollerRequest.
    
    - These classes no longer use RollerRequest:
         BaseRollerMenu
         EditorNavigationBarTag
         RollerMenuModel
         BigWeblogCalendarModel
         WeblogCalendarModel
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/branches/roller_3.0@421944 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/roller/ui/core/RequestConstants.java
@@ -0,0 +1,41 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+*  contributor license agreements.  The ASF licenses this file to You
+* under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.  For additional information regarding
+* copyright in this work, please see the NOTICE file in the top level
+* directory of this distribution.
+*/
+package org.apache.roller.ui.core;
+
+/**
+ * Static collection of request parameter names.
+ */
+public class RequestConstants {
+    public static final String ANCHOR            = "entry"; 
+    public static final String BOOKMARK_ID       = "bookmarkId"; 
+    public static final String FOLDER_ID         = "folderId"; 
+    public static final String PAGE_ID           = "pageId";
+    public static final String PARENT_ID         = "parentId";
+    public static final String PINGTARGET_ID     = "pingtargetId";
+    public static final String REFERRER_ID       = "referrerId";
+    public static final String USERNAME          = "username";
+    public static final String WEBLOG            = "weblog"; 
+    public static final String WEBLOG_ID         = "websiteId";
+    public static final String WEBLOGCATEGORY    = "cat";
+    public static final String WEBLOGCATEGORY_ID = "categoryId";
+    public static final String WEBLOGENTRY_ID    = "entryId";
+    
+    // this should ONLY be used in file-upload situations where 
+    // weblog handle cannot be conveyed as a request parameter
+    public static final String WEBLOG_SESSION_STASH = "weblog_session_stash";
+}
