a32a180484fe1c92a3888dd62bf496ac315afdb0
Allen T. Gilliland IV <agilliland@apache.org>
2006-07-17 21:29:22 +0000

    support both 2 char and 5 char locale strings for new multi-lang blogging feature.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/branches/roller_3.0@422884 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/rendering/WeblogRequestMapper.java
+++ src/org/apache/roller/ui/rendering/WeblogRequestMapper.java
@@ -338,13 +338,18 @@ public class WeblogRequestMapper implements RequestMapper {
         
         boolean isLocale = false;
         
-        // we only support 5 character locale strings, so check that first
-        if(potentialLocale != null && potentialLocale.length() == 5) {
+        // we only support 2 or 5 character locale strings, so check that first
+        if(potentialLocale != null && 
+                (potentialLocale.length() == 2 || potentialLocale.length() == 5)) {
             
             // now make sure that the format is proper ... e.g. "en_US"
             // we are not going to be picky about capitalization
             String[] langCountry = potentialLocale.split("_");
-            if(langCountry.length == 2 && 
+            if(langCountry.length == 1 && 
+                    langCountry[0] != null && langCountry[0].length() == 2) {
+                isLocale = true;
+                
+            } else if(langCountry.length == 2 && 
                     langCountry[0] != null && langCountry[0].length() == 2 && 
                     langCountry[1] != null && langCountry[1].length() == 2) {
                 
