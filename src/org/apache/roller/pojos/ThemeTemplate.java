a781a17675406f9ddc886a02bd12dfe02a027a19
Allen T. Gilliland IV <agilliland@apache.org>
2006-07-21 16:44:55 +0000

    code reformatting.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/branches/roller_3.0@424384 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/pojos/ThemeTemplate.java
+++ src/org/apache/roller/pojos/ThemeTemplate.java
@@ -1,24 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
-/*
- * ThemeTemplate.java
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Created on June 27, 2005, 12:14 PM
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
  */
 
 package org.apache.roller.pojos;
@@ -31,8 +26,6 @@ import org.apache.roller.pojos.Template;
 /**
  * A Theme based implementation of a Template.  A ThemeTemplate represents a
  * template which is part of a shared Theme.
- *
- * @author Allen Gilliland
  */
 public class ThemeTemplate implements Template, Serializable {
     
