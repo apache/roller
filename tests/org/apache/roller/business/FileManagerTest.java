7b67985c66f84357711357497f62d9301bdec447
Allen T. Gilliland IV <agilliland@apache.org>
2006-08-14 18:47:19 +0000

    fixing up unit tests.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/branches/roller_3.0@431407 13f79535-47bb-0310-9956-ffa450edef68

--- tests/org/apache/roller/business/FileManagerTest.java
+++ tests/org/apache/roller/business/FileManagerTest.java
@@ -1,20 +1,21 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ */
+
 package org.apache.roller.business;
 
 import java.io.InputStream;
@@ -94,7 +95,7 @@ public class FileManagerTest extends TestCase {
         // test quota functionality
         FileManager fmgr = RollerFactory.getRoller().getFileManager();
         RollerMessages msgs = new RollerMessages();
-        assertFalse(fmgr.canSave(testWeblog.getHandle(), "test.gif", 2500000, msgs));
+        assertFalse(fmgr.canSave(testWeblog.getHandle(), "test.gif", "text/plain", 2500000, msgs));
     }
     
     
@@ -116,7 +117,7 @@ public class FileManagerTest extends TestCase {
         
         // store a file
         InputStream is = getClass().getResourceAsStream("/bookmarks.opml");
-        fmgr.saveFile(testWeblog.getHandle(), "bookmarks.opml", 1545, is);
+        fmgr.saveFile(testWeblog.getHandle(), "bookmarks.opml", "text/plain", 1545, is);
         
         // make sure file was stored successfully
         assertEquals(1, fmgr.getFiles(testWeblog.getHandle()).length);
