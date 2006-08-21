b353adbdc1e2c2f9ecdf651ddf66701749e36e67
Allen T. Gilliland IV <agilliland@apache.org>
2006-08-21 17:35:58 +0000

    fixing calculation of absolute url path in init filter, which was broken in cases where app was deployed at root context and first request was to "/"
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/branches/roller_3.0@433299 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/core/filters/InitFilter.java
+++ src/org/apache/roller/ui/core/filters/InitFilter.java
@@ -78,8 +78,17 @@ public class InitFilter implements Filter {
         
         String fullUrl = request.getRequestURL().toString();
         
-        // first extract just the server portion of the url
-        url = fullUrl.substring(0, fullUrl.indexOf(request.getRequestURI()));
+        // if the uri is only "/" then we are basically done
+        if("/".equals(request.getRequestURI())) {
+            log.info(fullUrl.substring(0, fullUrl.length()-1));
+            return fullUrl.substring(0, fullUrl.length()-1);
+        }
+        
+        // find first "/" starting after hostname is specified
+        int index = fullUrl.indexOf("/", fullUrl.indexOf(request.getServerName()));
+        
+        // extract just the part leading up to uri
+        url = fullUrl.substring(0, index);
         
         // then just add on the context path
         url += request.getContextPath();
