def1d054a74230c2ccd55d88510cb2fd7d0c92db
Allen T. Gilliland IV <agilliland@apache.org>
2006-08-22 01:38:15 +0000

    cleanup handling of local feeds in planet for roller 3.0
    - separated out sync websites task and rank subscriptions task.
    - sync websites now uses roller 3.0 urls
    - getEntries() method for local roller planet impl now works with 3.0 urls
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/branches/roller_3.0@433455 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/core/tasks/SyncWebsitesTask.java
+++ src/org/apache/roller/ui/core/tasks/SyncWebsitesTask.java
@@ -18,43 +18,36 @@
 
 package org.apache.roller.ui.core.tasks;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.TimerTask;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.roller.RollerException;
-import org.apache.roller.config.RollerConfig;
 import org.apache.roller.config.RollerRuntimeConfig;
 import org.apache.roller.model.PlanetManager;
 import org.apache.roller.model.Roller;
 import org.apache.roller.model.RollerFactory;
 import org.apache.roller.model.ScheduledTask;
 import org.apache.roller.model.UserManager;
-import org.apache.roller.pojos.PlanetConfigData;
 import org.apache.roller.pojos.PlanetGroupData;
 import org.apache.roller.pojos.PlanetSubscriptionData;
-import org.apache.roller.pojos.UserData;
 import org.apache.roller.pojos.WebsiteData;
-import org.apache.roller.util.Technorati;
+import org.apache.roller.util.URLUtilities;
 
 
 /**
- * Ensure that every user is represented by a subscription in Planet Roller
- * database. Also "ranks" each subscription by populating Technorati inbound
- * blogs and links counts.
+ * Ensure that every weblog has a subscription in Planet Roller database.
  */
 public class SyncWebsitesTask extends TimerTask implements ScheduledTask {
     
-    private static Log logger = LogFactory.getLog(SyncWebsitesTask.class);
-    private Roller roller = null;
+    private static Log log = LogFactory.getLog(SyncWebsitesTask.class);
     
     
-    /** Task may be run from the command line */
+    /** 
+     * Task may be run from the command line 
+     */
     public static void main(String[] args) {
         try {
             RollerFactory.setRoller(
@@ -71,186 +64,96 @@ public class SyncWebsitesTask extends TimerTask implements ScheduledTask {
     
     
     public void init(Roller roller, String realPath) throws RollerException {
-        this.roller = roller;
+        // no-op
     }
     
     
     public void run() {
         syncWebsites();
-        rankSubscriptions();
     }
     
     
     /**
-     * Ensure there's a subscription in the "all" group for every Roller user.
+     * Ensure there's a subscription in the "all" group for every Roller weblog.
      */
     private void syncWebsites() {
-        try {
-            List liveUserFeeds = new ArrayList();
-            String baseURL = RollerRuntimeConfig.getProperty("site.absoluteurl");
-            if (baseURL == null || baseURL.trim().length()==0) {
-                logger.error("ERROR: cannot sync websites with Planet Roller - "
-                        +"absolute URL not specified in Roller Config");
-            } else {
-                PlanetManager planet = roller.getPlanetManager();
-                UserManager userManager = roller.getUserManager();
-                PlanetGroupData group = planet.getGroup("all");
-                if (group == null) {
-                    group = new PlanetGroupData();
-                    group.setHandle("all");
-                    group.setTitle("all");
-                    planet.saveGroup(group);
-                    roller.flush();
-                }
-                try {
-                    String baseFeedURL = baseURL + "/rss/";
-                    String baseSiteURL = baseURL + "/page/";
-                    // get list of all enabled and active weblogs
-                    Iterator websites =
-                            roller.getUserManager().getWebsites(null, Boolean.TRUE, Boolean.TRUE, null, null, 0, -1).iterator();
-                    while (websites.hasNext()) {
-                        WebsiteData website = (WebsiteData)websites.next();
-                        
-                        StringBuffer sitesb = new StringBuffer();
-                        sitesb.append(baseSiteURL);
-                        sitesb.append(website.getHandle());
-                        String siteUrl = sitesb.toString();
-                        
-                        StringBuffer feedsb = new StringBuffer();
-                        feedsb.append(baseFeedURL);
-                        feedsb.append(website.getHandle());
-                        String feedUrl = feedsb.toString();
-                        
-                        liveUserFeeds.add(feedUrl);
-                        
-                        PlanetSubscriptionData sub =
-                                planet.getSubscription(feedUrl);
-                        if (sub == null) {
-                            logger.info("ADDING feed: "+feedUrl);
-                            sub = new PlanetSubscriptionData();
-                            sub.setTitle(website.getName());
-                            sub.setFeedURL(feedUrl);
-                            sub.setSiteURL(siteUrl);
-                            sub.setAuthor(website.getHandle());
-                            planet.saveSubscription(sub);
-                            group.addSubscription(sub);
-                        } else {
-                            sub.setTitle(website.getName());
-                            sub.setAuthor(website.getHandle());
-                            planet.saveSubscription(sub);
-                        }
-                    }
-                    planet.saveGroup(group);
-                    roller.flush();
-                    roller.release();
-                    
-                    // TODO: new planet manager method deleteSubs(list)
-                    group = group = planet.getGroup("all");
-                    Iterator subs = group.getSubscriptions().iterator();
-                    while (subs.hasNext()) {
-                        PlanetSubscriptionData sub =
-                                (PlanetSubscriptionData)subs.next();
-                        if (!liveUserFeeds.contains(sub.getFeedURL())) {
-                            logger.info("DELETING feed: "+sub.getFeedURL());
-                            planet.deleteSubscription(sub);
-                        }
-                    }
-                    roller.flush();
-                } finally {
-                    roller.release();
-                }
-            }
-        } catch (RollerException e) {
-            logger.error("ERROR refreshing entries", e);
+        
+        // make sure we have an absolute url value
+        String absUrl = RollerRuntimeConfig.getProperty("site.absoluteurl");
+        if(absUrl == null || absUrl.trim().length() == 0) {
+            log.error("ERROR: cannot sync websites with Planet Roller - "
+                    +"absolute URL not specified in Roller Config");
+            return;
         }
-    }
-    
-    
-    /**
-     * Loop through all subscriptions get get Technorati rankings for each
-     */
-    private void rankSubscriptions() {
-        int count = 0;
-        int errorCount = 0;
+        
         try {
-            PlanetManager planet = roller.getPlanetManager();
-            PlanetConfigData config = planet.getConfiguration();
-            Technorati technorati = null;
-            try {
-                if (config.getProxyHost()!=null && config.getProxyPort() != -1) {
-                    technorati = new Technorati(
-                            config.getProxyHost(), config.getProxyPort());
+            PlanetManager planet = RollerFactory.getRoller().getPlanetManager();
+            UserManager userManager = RollerFactory.getRoller().getUserManager();
+            
+            // first, make sure there is an "all" planet group
+            PlanetGroupData group = planet.getGroup("all");
+            if(group == null) {
+                group = new PlanetGroupData();
+                group.setHandle("all");
+                group.setTitle("all");
+                planet.saveGroup(group);
+            }
+            
+            // walk through all enable weblogs and add/update subs as needed
+            List liveUserFeeds = new ArrayList();
+            Iterator websites =
+                    userManager.getWebsites(null, Boolean.TRUE, Boolean.TRUE, null, null, 0, -1).iterator();
+            while(websites.hasNext()) {
+                WebsiteData weblog = (WebsiteData) websites.next();
+                
+                String siteUrl = URLUtilities.getWeblogURL(weblog, null, true);
+                String feedUrl = URLUtilities.getWeblogFeedURL(weblog, null, "entries", "rss", null, false, true);
+                
+                // add feed url to the "live" list
+                liveUserFeeds.add(feedUrl);
+                
+                // if sub already exists then update it, otherwise add it
+                PlanetSubscriptionData sub = planet.getSubscription(feedUrl);
+                if (sub == null) {
+                    log.info("ADDING feed: "+feedUrl);
+                    
+                    sub = new PlanetSubscriptionData();
+                    sub.setTitle(weblog.getName());
+                    sub.setFeedURL(feedUrl);
+                    sub.setSiteURL(siteUrl);
+                    sub.setAuthor(weblog.getHandle());
+                    
+                    planet.saveSubscription(sub);
+                    group.addSubscription(sub);
                 } else {
-                    technorati = new Technorati();
+                    sub.setTitle(weblog.getName());
+                    sub.setAuthor(weblog.getHandle());
+                    
+                    planet.saveSubscription(sub);
                 }
-            } catch (IOException e) {
-                logger.error("Aborting collection of Technorati rankings.\n"
-                        +"technorati.license not found at root of classpath.\n"
-                        +"Get license at http://technorati.com/developers/apikey.html\n"
-                        +"Put the license string in a file called technorati.license.\n"
-                        +"And place that file at the root of Roller's classpath.\n"
-                        +"For example, in the /WEB-INF/classes directory.");
-                return;
             }
-            UserManager userManager = roller.getUserManager();
-            try {
-                int limit = RollerConfig.getIntProperty(
-                        "planet.aggregator.technorati.limit", 500);
-                int userCount = planet.getSubscriptionCount();
-                int mod = (userCount / limit) + 1;
-                
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(new Date());
-                int day = cal.get(Calendar.DAY_OF_YEAR);
-                
-                int start = (day % mod) * limit;
-                int end = start + limit;
-                end = end > userCount ? userCount : end;
-                logger.info("Updating subscriptions ["+start+":"+end+"]");
-                
-                Iterator subs = planet.getAllSubscriptions();
-                while (subs.hasNext()) {
-                    PlanetSubscriptionData sub =
-                            (PlanetSubscriptionData)subs.next();
-                    if (count >= start && count < end) {
-                        try {
-                            Technorati.Result result =
-                                    technorati.getBloginfo(sub.getSiteURL());
-                            if (result != null && result.getWeblog() != null) {
-                                sub.setInboundblogs(
-                                        result.getWeblog().getInboundblogs());
-                                sub.setInboundlinks(
-                                        result.getWeblog().getInboundlinks());
-                                logger.debug("Adding rank for "
-                                        +sub.getFeedURL()+" ["+count+"|"
-                                        +sub.getInboundblogs()+"|"
-                                        +sub.getInboundlinks()+"]");
-                            } else {
-                                logger.debug(
-                                        "No ranking available for "
-                                        +sub.getFeedURL()+" ["+count+"]");
-                                sub.setInboundlinks(0);
-                                sub.setInboundblogs(0);
-                            }
-                            planet.saveSubscription(sub);
-                        } catch (Exception e) {
-                            logger.warn("WARN ranking subscription ["
-                                    + count + "]: " + e.getMessage());
-                            if (errorCount++ > 5) {
-                                logger.warn(
-                                        "   Stopping ranking, too many errors");
-                                break;
-                            }
-                        }
-                    }
-                    count++;
+            
+            // new subs added, existing subs updated, now delete old subs
+            Iterator subs = group.getSubscriptions().iterator();
+            while(subs.hasNext()) {
+                PlanetSubscriptionData sub =
+                        (PlanetSubscriptionData) subs.next();
+                if (!liveUserFeeds.contains(sub.getFeedURL())) {
+                    log.info("DELETING feed: "+sub.getFeedURL());
+                    planet.deleteSubscription(sub);
+                    group.removeSubscription(sub);
                 }
-                roller.flush();
-            } finally {
-                roller.release();
             }
-        } catch (Exception e) {
-            logger.error("ERROR ranking subscriptions", e);
+            
+            // all done, lets save
+            planet.saveGroup(group);
+            RollerFactory.getRoller().flush();
+            
+        } catch (RollerException e) {
+            log.error("ERROR refreshing entries", e);
+        } finally {
+            // don't forget to release
+            RollerFactory.getRoller().release();
         }
     }
     
