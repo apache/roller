78fa2a365991f4a7cfc7ecc0437b770b05b9588f
Anil R. Gangolli <gangolli@apache.org>
2006-08-31 06:37:03 +0000

    Fix for Bug ROL-1225.  Add headers to get browsers and proxies not to cache.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/trunk@438817 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/rendering/servlets/CommentAuthenticatorServlet.java
+++ src/org/apache/roller/ui/rendering/servlets/CommentAuthenticatorServlet.java
@@ -63,6 +63,12 @@ public class CommentAuthenticatorServlet extends HttpServlet {
         throws IOException, ServletException {
         
         response.setContentType("text/html; charset=utf-8");
+
+        // Convince proxies and IE not to cache this.
+        response.addHeader("Pragma", "no-cache");
+        response.addHeader("Cache-Control", "no-cache");
+        response.addHeader("Expires", "Thu, 01 Jan 1970 00:00:00 GMT");
+
         PrintWriter out = response.getWriter();
         out.println(this.authenticator.getHtml(null, request, response));
     }
