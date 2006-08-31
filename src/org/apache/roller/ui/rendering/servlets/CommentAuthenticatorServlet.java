9f0214506040a26e508fcc9d99d6c2865ccd1da5
Anil R. Gangolli <gangolli@apache.org>
2006-08-31 06:45:58 +0000

    Fix for ROL-1225.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/branches/roller_3.0@438819 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/rendering/servlets/CommentAuthenticatorServlet.java
+++ src/org/apache/roller/ui/rendering/servlets/CommentAuthenticatorServlet.java
@@ -56,8 +56,14 @@ public class CommentAuthenticatorServlet extends HttpServlet {
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-        
+
         response.setContentType("text/html; charset=utf-8");
+
+        // Convince proxies and IE not to cache this.
+        response.addHeader("Pragma", "no-cache");
+        response.addHeader("Cache-Control", "no-cache");
+        response.addHeader("Expires", "Thu, 01 Jan 1970 00:00:00 GMT");
+
         PrintWriter out = response.getWriter();
         out.println(this.authenticator.getHtml(request));
     }
