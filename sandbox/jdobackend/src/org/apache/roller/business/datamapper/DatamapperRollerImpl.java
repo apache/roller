37041e31c560265891af2d35297236f54db93431
Allen T. Gilliland IV <agilliland@apache.org>
2006-09-05 23:08:06 +0000

    cleaning up Roller impl for datamapper and JDO.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/branches/roller_3.0@440523 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/jdobackend/src/org/apache/roller/business/datamapper/DatamapperRollerImpl.java
+++ sandbox/jdobackend/src/org/apache/roller/business/datamapper/DatamapperRollerImpl.java
@@ -15,13 +15,6 @@
  */
 
 package org.apache.roller.business.datamapper;
-
-/*
- * DatamapperRollerImpl.java
- *
- * Created on May 27, 2006, 2:50 PM
- *
- */
     
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,14 +22,15 @@ import org.apache.roller.RollerException;
 import org.apache.roller.business.RollerImpl;
 import org.apache.roller.model.BookmarkManager;
 import org.apache.roller.model.AutoPingManager;
+import org.apache.roller.model.ConfigManager;
 import org.apache.roller.model.PingQueueManager;
 import org.apache.roller.model.PingTargetManager;
 import org.apache.roller.model.PlanetManager;
 import org.apache.roller.model.PropertiesManager;
 import org.apache.roller.model.RefererManager;
-import org.apache.roller.model.Roller;
 import org.apache.roller.model.UserManager;
 import org.apache.roller.model.WeblogManager;
+
 
 /**
  * A Datamapper specific implementation of the Roller business layer.
@@ -45,8 +39,7 @@ public abstract class DatamapperRollerImpl extends RollerImpl {
 
     static final long serialVersionUID = 5256135928578074652L;
 
-    protected static Log logger = 
-            LogFactory.getLog(DatamapperRollerImpl.class);
+    protected static Log logger = LogFactory.getLog(DatamapperRollerImpl.class);
 
     // our singleton instance
     private static DatamapperRollerImpl me = null;
@@ -65,13 +58,16 @@ public abstract class DatamapperRollerImpl extends RollerImpl {
     private AutoPingManager autoPingManager = null;
     private PingTargetManager pingTargetManager = null;
 
+    
     protected DatamapperRollerImpl() throws RollerException {
     }
 
+    
     public void flush() throws RollerException {
         this.strategy.flush();
     }
 
+    
     public void release() {
 
         // release our own stuff first
@@ -90,6 +86,7 @@ public abstract class DatamapperRollerImpl extends RollerImpl {
         super.release();
     }
 
+    
     public void shutdown() {
 
         // do our own shutdown first
@@ -99,139 +96,115 @@ public abstract class DatamapperRollerImpl extends RollerImpl {
         super.shutdown();
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getAutoPingManager()
      */
     public AutoPingManager getAutopingManager() throws RollerException {
         if (autoPingManager == null) {
-            autoPingManager = createAutoPingManagerImpl(strategy);
+            autoPingManager = new DatamapperAutoPingManagerImpl(strategy);
         }
         return autoPingManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getBookmarkManager()
      */
     public BookmarkManager getBookmarkManager() throws RollerException {
         if ( bookmarkManager == null ) {
-            bookmarkManager = createBookmarkManagerImpl(strategy);
+            bookmarkManager = new DatamapperBookmarkManagerImpl(strategy);
         }
         return bookmarkManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getPingTargetManager()
      */
     public PingQueueManager getPingQueueManager() throws RollerException {
         if (pingQueueManager == null) {
-            pingQueueManager = createPingQueueManagerImpl(strategy);
+            pingQueueManager = new DatamapperPingQueueManagerImpl(strategy);
         }
         return pingQueueManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getPingTargetManager()
      */
     public PingTargetManager getPingTargetManager() throws RollerException {
         if (pingTargetManager == null) {
-            pingTargetManager = createPingTargetManagerImpl(strategy);
+            pingTargetManager = new DatamapperPingTargetManagerImpl(strategy);
         }
         return pingTargetManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getPlanetManager()
      */
     public PlanetManager getPlanetManager() throws RollerException {
         if ( planetManager == null ) {
-            planetManager = createPlanetManagerImpl(strategy);
+            planetManager = new DatamapperPlanetManagerImpl(strategy);
         }
         return planetManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getPropertiesManager()
      */
     public PropertiesManager getPropertiesManager() throws RollerException {
         if (propertiesManager == null) {
-            propertiesManager = createPropertiesManagerImpl(strategy);
+            propertiesManager = new DatamapperPropertiesManagerImpl(strategy);
         }
         return propertiesManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getRefererManager()
      */
     public RefererManager getRefererManager() throws RollerException {
         if ( referrerManager == null ) {
-            referrerManager = createReferrerManagerImpl(strategy);
+            referrerManager = new DatamapperReferrerManagerImpl(strategy);
         }
         return referrerManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getUserManager()
      */
     public UserManager getUserManager() throws RollerException {
         if ( userManager == null ) {
-            userManager = createUserManagerImpl(strategy);
+            userManager = new DatamapperUserManagerImpl(strategy);
         }
         return userManager;
     }
 
+    
     /**
      * @see org.apache.roller.model.Roller#getWeblogManager()
      */
     public WeblogManager getWeblogManager() throws RollerException {
         if ( weblogManager == null ) {
-            weblogManager = createWeblogManagerImpl(strategy);
+            weblogManager = new DatamapperWeblogManagerImpl(strategy);
         }
         return weblogManager;
     }
-
-    protected AutoPingManager createAutoPingManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperAutoPingManagerImpl(strategy);
+    
+    
+    /**
+     * This method is deprecated.
+     * @return null
+     * @see org.apache.roller.model.Roller#getConfigManager()
+     * @deprecated see JIRA issue ROL-1151
+     * @throws org.apache.roller.RollerException on any error
+     */
+    public ConfigManager getConfigManager() throws RollerException {
+        throw new RollerException("Deprecated method getConfigManager.");
     }
-
-    protected BookmarkManager createBookmarkManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperBookmarkManagerImpl(strategy);
-    }
-
-    protected PingTargetManager createPingTargetManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperPingTargetManagerImpl(strategy);
-    }
-
-    protected PingQueueManager createPingQueueManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperPingQueueManagerImpl(strategy);
-    }
-
-    protected PlanetManager createPlanetManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperPlanetManagerImpl(strategy);
-    }
-
-    protected PropertiesManager createPropertiesManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperPropertiesManagerImpl(strategy);
-    }
-
-    protected RefererManager createReferrerManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperReferrerManagerImpl(strategy);
-    }
-
-    protected UserManager createUserManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperUserManagerImpl(strategy);
-    }
-
-    protected WeblogManager createWeblogManagerImpl
-            (DatamapperPersistenceStrategy strategy) {
-        return new DatamapperWeblogManagerImpl(strategy);
-    }
-
+    
 }
