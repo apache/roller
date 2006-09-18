f2b2dc903a767bb1f144b82681f8cecc55bc9cff
Allen T. Gilliland IV <agilliland@apache.org>
2006-09-18 21:30:16 +0000

    code reformatting.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/trunk@447573 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/pojos/Theme.java
+++ src/org/apache/roller/pojos/Theme.java
@@ -1,24 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
-/*
- * Theme.java
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Created on June 27, 2005, 12:55 PM
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
  */
 
 package org.apache.roller.pojos;
@@ -35,8 +30,6 @@ import java.util.Map;
  * The Theme object encapsulates all elements of a single weblog theme.  It
  * is used mostly to contain all the templates for a theme, but does contain
  * other theme related attributes such as name, last modifed date, etc.
- *
- * @author Allen Gilliland
  */
 public class Theme implements Serializable {
     
