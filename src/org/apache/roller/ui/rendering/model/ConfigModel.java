e36963a7616fee817192628f03c75ee91987019a
Allen T. Gilliland IV <agilliland@apache.org>
2006-10-02 21:53:26 +0000

    removing unneeded reverences to javax.servlet.jsp classes.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/roller/trunk@452235 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/rendering/model/ConfigModel.java
+++ src/org/apache/roller/ui/rendering/model/ConfigModel.java
@@ -23,7 +23,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.servlet.jsp.PageContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.roller.RollerException;
