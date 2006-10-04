d228592093d63bf17af31692c428c5f52e580468
David M. Johnson <snoopdave@apache.org>
2006-10-04 19:45:53 +0000

    PlanetConfigAction is an admin action
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@452990 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/admin/struts/actions/PlanetConfigAction.java
+++ src/org/apache/roller/ui/admin/struts/actions/PlanetConfigAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.roller.ui.authoring.struts.actions;
+package org.apache.roller.ui.admin.struts.actions;
 
 import java.io.IOException;
 
