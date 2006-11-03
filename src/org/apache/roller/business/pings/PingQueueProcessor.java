2f0f73fbe15eb2255628a073793c23d3c7ff88d2
Allen T. Gilliland IV <agilliland@apache.org>
2006-11-03 00:25:00 +0000

    code reformatting.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@470615 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/business/pings/PingQueueProcessor.java
+++ src/org/apache/roller/business/pings/PingQueueProcessor.java
@@ -16,22 +16,20 @@
  * directory of this distribution.
  */
 
-
 package org.apache.roller.business.pings;
 
+import java.util.Iterator;
+import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.roller.RollerException;
+import org.apache.roller.business.RollerFactory;
 import org.apache.roller.config.PingConfig;
 import org.apache.roller.config.RollerRuntimeConfig;
-import org.apache.roller.business.pings.PingQueueManager;
-import org.apache.roller.business.RollerFactory;
 import org.apache.roller.pojos.PingQueueEntryData;
 import org.apache.roller.pojos.PingTargetData;
 import org.apache.roller.pojos.WebsiteData;
 
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Ping Queue Processor.  Singleton encapsulating logic for processing the weblog update ping queue.
@@ -39,21 +37,24 @@ import java.util.List;
  * @author <a href="mailto:anil@busybuddha.org">Anil Gangolli</a>
  */
 public class PingQueueProcessor {
+    
     private static final Log logger = LogFactory.getLog(PingQueueProcessor.class);
-
+    
     private static PingQueueProcessor theInstance;
-
-
+    
     private PingQueueManager pingQueueMgr;
-
+    
+    
     public static PingQueueProcessor getInstance() {
         return theInstance;
     }
-
+    
+    
     private PingQueueProcessor() throws RollerException {
         pingQueueMgr = RollerFactory.getRoller().getPingQueueManager();
     }
-
+    
+    
     /**
      * Initialize the singleton.  This is called during <code>RollerContext</code> initialization.
      *
@@ -67,7 +68,8 @@ public class PingQueueProcessor {
         theInstance = new PingQueueProcessor();
         if (logger.isDebugEnabled()) logger.debug("Ping queue processor initialized.");
     }
-
+    
+    
     /**
      * Process the ping queue.  Performs one pass through the ping queue, processing every entry once.  On ping failure
      * an entry is requeued for processing on subsequent passes until the configured maximum number of attempts is
@@ -78,23 +80,23 @@ public class PingQueueProcessor {
             logger.info("Ping processing has been suspended.  Skipping current round of ping queue processing.");
             return;
         }
-
+        
         String absoluteContextUrl = RollerRuntimeConfig.getAbsoluteContextURL();
         if (absoluteContextUrl == null) {
             logger.warn("WARNING: Skipping current ping queue processing round because we cannot yet determine the site's absolute context url.");
             return;
         }
-
+        
         // TODO: Group by ping target and ping all sites for that target?
         // We're currently not taking advantage of grouping by ping target site and then sending
         // all of the pings for that target at once.  If it becomes an efficiency issue, we should do
         // that.
-
+        
         try {
             if (logger.isDebugEnabled()) logger.debug("Started processing ping queue.");
             // Get all of the entries
             List entries = pingQueueMgr.getAllQueueEntries();
-
+            
             // Process each entry
             for (Iterator i = entries.iterator(); i.hasNext();) {
                 PingQueueEntryData pingQueueEntry = (PingQueueEntryData) i.next();
@@ -105,7 +107,8 @@ public class PingQueueProcessor {
             logger.error("Unexpected exception processing ping queue!  Aborting this pass of ping queue processing.", ex);
         }
     }
-
+    
+    
     /**
      * Process an individual ping queue entry.
      *
@@ -115,7 +118,7 @@ public class PingQueueProcessor {
      */
     private void processQueueEntry(PingQueueEntryData pingQueueEntry) throws RollerException {
         if (logger.isDebugEnabled()) logger.debug("Processing ping queue entry: " + pingQueueEntry);
-
+        
         PingTargetData pingTarget = pingQueueEntry.getPingTarget();
         WebsiteData website = pingQueueEntry.getWebsite();
         boolean pingSucceeded = false;
@@ -142,7 +145,8 @@ public class PingQueueProcessor {
             pingQueueMgr.removeQueueEntry(pingQueueEntry);
         }
     }
-
+    
+    
     /**
      * Handle any ping error.
      *
@@ -150,9 +154,11 @@ public class PingQueueProcessor {
      * @param ex             the exception that occurred on the ping attempt
      * @throws RollerException
      */
-    private void handlePingError(PingQueueEntryData pingQueueEntry, Exception ex) throws RollerException {
-        if ((pingQueueEntry.incrementAttempts() < PingConfig.getMaxPingAttempts()) && WeblogUpdatePinger.shouldRetry(ex))
-        {
+    private void handlePingError(PingQueueEntryData pingQueueEntry,
+            Exception ex)
+            throws RollerException {
+        
+        if ((pingQueueEntry.incrementAttempts() < PingConfig.getMaxPingAttempts()) && WeblogUpdatePinger.shouldRetry(ex)) {
             // We have attempts remaining, and it looks like we should retry,
             // so requeue the entry for processing on subsequent rounds
             logger.warn("Error on ping attempt (" + pingQueueEntry.getAttempts() + ") for " + pingQueueEntry + ": [" + ex.getMessage() + "]. Will re-queue for later attempts.");
@@ -166,6 +172,5 @@ public class PingQueueProcessor {
             // TODO: mark ping target invalid?
         }
     }
-
-
+    
 }
