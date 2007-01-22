f788f934da70b465e57e931358479b5fb2a43250
Allen T. Gilliland IV <agilliland@apache.org>
2007-01-22 18:42:34 +0000

    fix up PlanetFactory to ensure there is no possible double instantiation of Planet implemenation.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/roller/trunk@498741 13f79535-47bb-0310-9956-ffa450edef68

--- apps/planet/src/java/org/apache/roller/planet/business/PlanetFactory.java
+++ apps/planet/src/java/org/apache/roller/planet/business/PlanetFactory.java
@@ -15,25 +15,78 @@
  * copyright in this work, please see the NOTICE file in the top level
  * directory of this distribution.
  */
-package org.apache.roller.planet.business;
 
+package org.apache.roller.planet.business;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.roller.planet.config.PlanetConfig; 
 import org.apache.commons.lang.StringUtils;
 
+
 /**
  * Instantiates planet implementation.
  */
 public abstract class PlanetFactory {
+    
     private static final String DEFAULT_IMPL =
         "org.apache.roller.planet.business.hibernate.HibernatePlanetImpl";
     
     private static Planet planetInstance = null;
     
-    private static Log mLogger =
-            LogFactory.getFactory().getInstance(PlanetFactory.class);
+    private static Log log = LogFactory.getLog(PlanetFactory.class);
+    
+    
+    /**
+     * We instantiate the Planet implementation statically at class loading time
+     * to make absolutely sure that there is no way for our singleton to get
+     * instantiated twice.
+     */
+    static {
+        // lookup value for the roller classname to use
+        String planet_classname =
+                PlanetConfig.getProperty("persistence.planet.classname");
+        if(planet_classname == null || planet_classname.trim().length() < 1)
+            planet_classname = DEFAULT_IMPL;
+        
+        try {
+            Class planetClass = Class.forName(planet_classname);
+            java.lang.reflect.Method instanceMethod =
+                    planetClass.getMethod("instantiate", (Class[])null);
+            
+            // do the invocation
+            planetInstance = (Planet) instanceMethod.invoke(planetClass, (Object[])null);
+            
+            log.info("Using Planet Impl: " + planet_classname);
+            
+        } catch (Throwable e) {
+            
+            // uh oh
+            log.error("Error instantiating " + planet_classname, e);
+            
+            try {
+                // if we didn't already try DEFAULT_IMPL then try it now
+                if( ! DEFAULT_IMPL.equals(planet_classname)) {
+                    
+                    log.info("** Trying DEFAULT_IMPL "+DEFAULT_IMPL+" **");
+                    
+                    Class rollerClass = Class.forName(DEFAULT_IMPL);
+                    java.lang.reflect.Method instanceMethod =
+                            rollerClass.getMethod("instantiate", (Class[])null);
+                    
+                    // do the invocation
+                    planetInstance = (Planet) instanceMethod.invoke(rollerClass, (Object[])null);
+                } else {
+                    // we just do this so that the logger gets the message
+                    throw new Exception("Doh! Couldn't instantiate a planet class");
+                }
+                
+            } catch (Exception re) {
+                log.fatal("Failed to instantiate fallback planet impl", re);
+            }
+        }
+    }
+    
     
     /**
      * Let's just be doubling certain this class cannot be instantiated.
@@ -43,85 +96,9 @@ public abstract class PlanetFactory {
         // hello planetary citizens
     }
     
-    /**
-     * Static accessor for the instance of Planet
-     * held by this class.
-     *
-     * @return Planet
-     */
+    
     public static Planet getPlanet() {
-        // check to see if we need to instantiate
-        if(planetInstance == null) {
-            // lookup value for the planet classname to use
-            String planet_classname =
-                    PlanetConfig.getProperty("persistence.planet.classname");
-            
-            // now call setPlanet ourselves
-            PlanetFactory.setPlanet(planet_classname);
-        }
-        
         return planetInstance;
     }
     
-    
-    /**
-     * Construct the actual Planet implemenation for this instance.
-     *
-     * Use reflection to call the implementation's instantiate() method.
-     * Should this fail (either no implementation is specified or
-     * instantiate() throws an Exception) then the DEFAULT_IMPL will be tried.
-     * If this should fail then we are in trouble :/
-     *
-     * @param planet_classname The name of the Planet implementation class
-     * to instantiate.
-     */
-    public static void setPlanet(String planet_classname) {
-        
-        if (StringUtils.isEmpty( planet_classname ))
-            planet_classname = DEFAULT_IMPL;
-        
-        try {
-            Class planetClass = Class.forName(planet_classname);
-            java.lang.reflect.Method instanceMethod =
-                    planetClass.getMethod("instantiate", (Class[])null);
-            
-            // do the invocation
-            planetInstance = (Planet)
-                instanceMethod.invoke(planetClass, (Object[])null);
-            
-            mLogger.info("Using Planet Impl: " + planet_classname);
-        } catch (Exception e) {
-            // uh oh
-            mLogger.error("Error instantiating " + planet_classname, e);
-            try {
-                // if we didn't already try DEFAULT_IMPL then try it now
-                if( ! DEFAULT_IMPL.equals(planet_classname)) {
-                    mLogger.info("** Trying DEFAULT_IMPL "+DEFAULT_IMPL+" **");
-                    
-                    Class planetClass = Class.forName(DEFAULT_IMPL);
-                    java.lang.reflect.Method instanceMethod =
-                            planetClass.getMethod("instantiate", (Class[])null);
-                    
-                    // do the invocation
-                    planetInstance = (Planet)
-                        instanceMethod.invoke(planetClass, (Object[])null);
-                } else {
-                    // we just do this so that the logger gets the message
-                    throw new Exception("Doh! Couldn't instantiate the planet class");
-                }
-                
-            } catch (Exception re) {
-                mLogger.fatal("Failed to instantiate fallback planet impl", re);
-            }
-        }
-        
-    }
-    
-    
-    /**
-     * Set Planet to be returned by factory.
-     */
-    public static void setPlanet(Planet planet) {
-        if (planet != null) planetInstance = planet;
-    }
 }
