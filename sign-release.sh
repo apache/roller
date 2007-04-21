7a3895482e6075679b25b546a150ea3e6dd6adc8
David M. Johnson <snoopdave@apache.org>
2007-04-21 14:04:15 +0000

    Fixing incorrect reference to $entry in themes, preparing for 3.0.1 RC3
    git-svn-id: https://svn.apache.org/repos/asf/roller/branches/roller_3.0@531032 13f79535-47bb-0310-9956-ffa450edef68

--- sign-release.sh
+++ sign-release.sh
@@ -1,16 +1,16 @@
 
-export rcstring="-rc1"
+export rcstring="-rc3"
 
-cp dist/apache-roller-3.0-incubating.tar.gz dist/apache-roller-3.0${rcstring}-incubating.tar.gz
-cp dist/apache-roller-3.0-incubating.zip    dist/apache-roller-3.0${rcstring}-incubating.zip
+cp dist/apache-roller-3.0.1-incubating.tar.gz dist/apache-roller-3.0.1${rcstring}-incubating.tar.gz
+cp dist/apache-roller-3.0.1-incubating.zip    dist/apache-roller-3.0.1${rcstring}-incubating.zip
 
-cp dist/apache-roller-src-3.0-incubating.tar.gz  dist/apache-roller-src-3.0${rcstring}-incubating.tar.gz
-cp dist/apache-roller-src-3.0-incubating.zip     dist/apache-roller-src-3.0${rcstring}-incubating.zip
+cp dist/apache-roller-src-3.0.1-incubating.tar.gz  dist/apache-roller-src-3.0.1${rcstring}-incubating.tar.gz
+cp dist/apache-roller-src-3.0.1-incubating.zip     dist/apache-roller-src-3.0.1${rcstring}-incubating.zip
 
-gpg --armor --output dist/apache-roller-3.0-incubating.tar.gz.asc     --detach-sig dist/apache-roller-3.0${rcstring}-incubating.tar.gz
-gpg --armor --output dist/apache-roller-3.0-incubating.zip.asc        --detach-sig dist/apache-roller-3.0${rcstring}-incubating.zip
+gpg --armor --output dist/apache-roller-3.0.1-incubating.tar.gz.asc     --detach-sig dist/apache-roller-3.0.1${rcstring}-incubating.tar.gz
+gpg --armor --output dist/apache-roller-3.0.1-incubating.zip.asc        --detach-sig dist/apache-roller-3.0.1${rcstring}-incubating.zip
 
-gpg --armor --output dist/apache-roller-src-3.0-incubating.tar.gz.asc --detach-sig dist/apache-roller-src-3.0${rcstring}-incubating.tar.gz
-gpg --armor --output dist/apache-roller-src-3.0-incubating.zip.asc    --detach-sig dist/apache-roller-src-3.0${rcstring}-incubating.zip
+gpg --armor --output dist/apache-roller-src-3.0.1-incubating.tar.gz.asc --detach-sig dist/apache-roller-src-3.0.1${rcstring}-incubating.tar.gz
+gpg --armor --output dist/apache-roller-src-3.0.1-incubating.zip.asc    --detach-sig dist/apache-roller-src-3.0.1${rcstring}-incubating.zip
 
 
