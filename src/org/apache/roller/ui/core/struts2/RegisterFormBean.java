714e9517edcfe5e2704c3e67dc216adb75b177ef
Allen T. Gilliland IV <agilliland@apache.org>
2007-04-28 21:39:05 +0000

    some cleanup in register form bean.
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@533428 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/roller/ui/core/struts2/RegisterFormBean.java
+++ src/org/apache/roller/ui/core/struts2/RegisterFormBean.java
@@ -29,166 +29,101 @@ import org.apache.roller.pojos.UserData;
  */
 public class RegisterFormBean {
     
-    protected java.lang.Boolean enabled;
-    protected java.lang.String id;
-    protected java.lang.String userName;
-    protected java.lang.String password;
-    protected java.lang.String fullName;
-    protected java.lang.String emailAddress;
-    protected java.lang.String locale;
-    protected java.lang.String timeZone;
-    protected java.lang.String activationCode;
+    private String id = null;
+    private String userName = null;
+    private String password = null;
+    private String fullName = null;
+    private String emailAddress = null;
+    private String locale = null;
+    private String timeZone = null;
     private String mPasswordText = null;
     private String mPasswordConfirm = null;
+
     
-    
-    public java.lang.Boolean getEnabled() {
-        return this.enabled;
+    public String getId() {
+        return id;
     }
-    
-    /**
-     */
-    public void setEnabled( java.lang.Boolean enabled ) {
-        this.enabled = enabled;
-    }
-    
-    public java.lang.String getId() {
-        return this.id;
-    }
-    
-    /**
-     * @struts.validator type="required" msgkey="errors.required"
-     */
-    public void setId( java.lang.String id ) {
+
+    public void setId(String id) {
         this.id = id;
     }
-    
-    public java.lang.String getUserName() {
-        return this.userName;
+
+    public String getUserName() {
+        return userName;
     }
-    
-    /**
-     */
-    public void setUserName( java.lang.String userName ) {
+
+    public void setUserName(String userName) {
         this.userName = userName;
     }
-    
-    public java.lang.String getPassword() {
-        return this.password;
+
+    public String getPassword() {
+        return password;
     }
-    
-    /**
-     */
-    public void setPassword( java.lang.String password ) {
+
+    public void setPassword(String password) {
         this.password = password;
     }
-    
-    public java.lang.String getFullName() {
-        return this.fullName;
+
+    public String getFullName() {
+        return fullName;
     }
-    
-    /**
-     */
-    public void setFullName( java.lang.String fullName ) {
+
+    public void setFullName(String fullName) {
         this.fullName = fullName;
     }
-    
-    public java.lang.String getEmailAddress() {
-        return this.emailAddress;
+
+    public String getEmailAddress() {
+        return emailAddress;
     }
-    
-    /**
-     */
-    public void setEmailAddress( java.lang.String emailAddress ) {
+
+    public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
-    
-    public java.lang.String getLocale() {
-        return this.locale;
+
+    public String getLocale() {
+        return locale;
     }
-    
-    /**
-     */
-    public void setLocale( java.lang.String locale ) {
+
+    public void setLocale(String locale) {
         this.locale = locale;
     }
-    
-    public java.lang.String getTimeZone() {
-        return this.timeZone;
+
+    public String getTimeZone() {
+        return timeZone;
     }
-    
-    /**
-     */
-    public void setTimeZone( java.lang.String timeZone ) {
+
+    public void setTimeZone(String timeZone) {
         this.timeZone = timeZone;
     }
     
-    public java.lang.String getActivationCode() {
-        return this.activationCode;
-    }
-    
-    /**
-     */
-    public void setActivationCode( java.lang.String activationCode ) {
-        this.activationCode = activationCode;
-    }
-    
-    /**
-     * Don't call it "password" because browser will autofill.
-     * @return Returns the passwordText.
-     */
     public String getPasswordText() {
         return mPasswordText;
     }
     
-    /**
-     * Don't call it "password" because browser will autofill.
-     * @param passwordText The passwordText to set.
-     */
     public void setPasswordText(String passwordText) {
         mPasswordText = passwordText;
     }
     
-    /**
-     * @return Returns the passwordConfirm.
-     */
     public String getPasswordConfirm() {
         return mPasswordConfirm;
     }
     
-    /**
-     * @param passwordConfirm The passwordConfirm to set.
-     */
     public void setPasswordConfirm(String passwordConfirm) {
         mPasswordConfirm = passwordConfirm;
     }
     
     
-    /**
-     * Copy values from this form bean to the specified data object.
-     * Only copies primitive types (Boolean, boolean, String, Integer, int, Timestamp, Date)
-     */
     public void copyTo(UserData dataHolder, Locale locale) {
         
-        dataHolder.setEnabled(this.enabled);
-        dataHolder.setId(this.id);
-        dataHolder.setUserName(this.userName);
         dataHolder.setFullName(this.fullName);
         dataHolder.setEmailAddress(this.emailAddress);
         dataHolder.setLocale(this.locale);
         dataHolder.setTimeZone(this.timeZone);
-        
-        dataHolder.setActivationCode(this.activationCode);
     }
     
     
-    /**
-     * Copy values from specified data object to this form bean.
-     * Includes all types.
-     */
     public void copyFrom(UserData dataHolder, Locale locale) {
         
-        this.enabled = dataHolder.getEnabled();
         this.id = dataHolder.getId();
         this.userName = dataHolder.getUserName();
         this.password = dataHolder.getPassword();
@@ -196,25 +131,6 @@ public class RegisterFormBean {
         this.emailAddress = dataHolder.getEmailAddress();
         this.locale = dataHolder.getLocale();
         this.timeZone = dataHolder.getTimeZone();
-        
-        this.activationCode = dataHolder.getActivationCode();
-        
-    }
-    
-    
-    public void doReset() {
-        
-        this.enabled = null;
-        this.id = null;
-        this.userName = null;
-        this.password = null;
-        this.fullName = null;
-        this.emailAddress = null;
-        this.locale = null;
-        this.timeZone = null;
-        
-        this.activationCode = null;
-        
     }
     
 }
