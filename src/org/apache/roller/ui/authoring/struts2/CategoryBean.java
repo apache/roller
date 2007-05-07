d7533b6333f14651ed05cc8a3cb45151ea8171b5
Allen T. Gilliland IV <agilliland@apache.org>
2007-05-07 23:23:17 +0000

    struts2 migration for categories actions.
    git-svn-id: https://svn.eu.apache.org/repos/asf/roller/trunk@536017 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/roller/ui/authoring/struts2/CategoryBean.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ */
+
+package org.apache.roller.ui.authoring.struts2;
+
+import org.apache.roller.RollerException;
+import org.apache.roller.pojos.WeblogCategoryData;
+
+
+/**
+ * Bean for managing category data.
+ */
+public class CategoryBean {
+    
+    private String id = null;
+    private String name = null;
+    private String description = null;
+    private String image = null;
+    
+    
+    public String getId() {
+        return this.id;
+    }
+    
+    public void setId( String id ) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName( String name ) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public void setDescription( String description ) {
+        this.description = description;
+    }
+    
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    public void copyTo(WeblogCategoryData dataHolder) throws RollerException {
+        
+        if(!dataHolder.getName().equals(this.name)) {
+            dataHolder.updateName(this.name);
+        }
+        
+        dataHolder.setDescription(this.description);
+        dataHolder.setImage(this.image);
+    }
+    
+    
+    public void copyFrom(WeblogCategoryData dataHolder) {
+        this.id = dataHolder.getId();
+        this.name = dataHolder.getName();
+        this.description = dataHolder.getDescription();
+        this.image = dataHolder.getImage();
+    }
+    
+}
