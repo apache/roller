d22160863f56e92e9ecfe3e95e140fd1d73d40a0
Allen T. Gilliland IV <agilliland@apache.org>
2007-05-15 21:37:02 +0000

    Utility code for dealing with external mediacast resources attached to entries.
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@538329 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/roller/util/MediacastException.java
@@ -0,0 +1,53 @@
+/*
+ * MediacastException.java
+ * 
+ * Created on May 11, 2007, 3:53:28 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.apache.roller.util;
+
+import org.apache.roller.RollerException;
+
+
+/**
+ * An exception thrown when dealing with Mediacast files.
+ */
+public class MediacastException extends RollerException {
+    
+    private int errorCode = 0;
+    private String errorKey = null;
+    
+    
+    public MediacastException(int code, String msgKey) {
+        this.errorCode = code;
+        this.errorKey = msgKey;
+    }
+    
+    
+    public MediacastException(int code, String msgKey, Throwable t) {
+        super(t);
+        this.errorCode = code;
+        this.errorKey = msgKey;
+    }
+    
+    
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorKey() {
+        return errorKey;
+    }
+    
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public void setErrorKey(String errorKey) {
+        this.errorKey = errorKey;
+    }
+    
+}
