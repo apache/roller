e0255673a14e01bf13236f0a92507382fe512eee
David M. Johnson <snoopdave@apache.org>
2007-05-23 21:17:05 +0000

    Roller's own Acegi user details service, enables Acegi authentication to work regardless of how Roller's database provider is configured.
    git-svn-id: https://svn.eu.apache.org/repos/asf/roller/trunk@541089 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/roller/ui/core/security/RollerUserDetailsService.java
@@ -0,0 +1,52 @@
+package org.apache.roller.ui.core.security;
+
+import java.util.Iterator;
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
+import org.acegisecurity.userdetails.User;
+import org.acegisecurity.userdetails.UserDetails;
+import org.acegisecurity.userdetails.UserDetailsService;
+import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.apache.roller.RollerException;
+import org.apache.roller.business.Roller;
+import org.apache.roller.business.RollerFactory;
+import org.apache.roller.business.UserManager;
+import org.apache.roller.pojos.RoleData;
+import org.apache.roller.pojos.UserData;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataRetrievalFailureException;
+
+/**
+ * Acegi user details service implemented using Roller API.
+ */
+public class RollerUserDetailsService implements UserDetailsService {
+
+    public UserDetails loadUserByUsername(String userName) 
+        throws UsernameNotFoundException, DataAccessException {
+        
+        UserData userData = null;
+        try {
+            Roller roller = RollerFactory.getRoller();
+            UserManager umgr = roller.getUserManager();
+            userData = umgr.getUserByUserName(userName, Boolean.TRUE); 
+        } catch (RollerException ex) {
+            throw new DataRetrievalFailureException("ERROR in user lookup", ex);
+        } 
+        
+        if (userData == null) {
+            throw new UsernameNotFoundException("ERROR no user: " + userName);
+        }
+        
+        GrantedAuthority[] authorities = 
+            new GrantedAuthorityImpl[userData.getRoles().size()];
+        int i = 0;
+        for (Iterator it = userData.getRoles().iterator(); it.hasNext();) {
+            RoleData role = (RoleData)it.next();
+            authorities[i++] = new GrantedAuthorityImpl(role.getRole());
+        }
+        
+        return new User(
+            userData.getUserName(), userData.getPassword(), true, authorities);
+    }
+    
+}
