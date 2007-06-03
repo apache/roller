6ba896431d6a796b424129a6efed699ad1878536
Allen T. Gilliland IV <agilliland@apache.org>
2007-06-03 22:02:08 +0000

    modify getActionURL() method so that it can receive a map of extra parameters to use for building the url.
    git-svn-id: https://svn.eu.apache.org/repos/asf/roller/trunk@544001 13f79535-47bb-0310-9956-ffa450edef68

--- apps/weblogger/src/java/org/apache/roller/weblogger/ui/rendering/model/URLModel.java
+++ apps/weblogger/src/java/org/apache/roller/weblogger/ui/rendering/model/URLModel.java
@@ -107,11 +107,11 @@ public class URLModel implements Model {
     public String action(String action, String namespace) {
         if(namespace != null) {
             if("/roller-ui".equals(namespace)) {
-                return URLUtilities.getActionURL(action, namespace, null, true);
+                return URLUtilities.getActionURL(action, namespace, null, null, true);
             } else if("/roller-ui/authoring".equals(namespace)) {
-                return URLUtilities.getActionURL(action, namespace, weblog.getHandle(), true);
+                return URLUtilities.getActionURL(action, namespace, weblog.getHandle(), null, true);
             } else if("/roller-ui/admin".equals(namespace)) {
-                return URLUtilities.getActionURL(action, namespace, null, true);
+                return URLUtilities.getActionURL(action, namespace, null, null, true);
             }
         }
         return null;
