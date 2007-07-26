7a87eb3fbf8abcc2ec1febb02b2ede9d043b8b08
David M. Johnson <snoopdave@apache.org>
2007-07-26 17:04:34 +0000

    Better fix for ROL-548 - Bookmark display macro not obeying sort order
    This one does not introduce a new getBookmarks() method, instead the folder wrapper's getBookmarks() method does the sorting.
    
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@559884 13f79535-47bb-0310-9956-ffa450edef68

--- apps/weblogger/src/java/org/apache/roller/weblogger/pojos/wrapper/WeblogBookmarkFolderWrapper.java
+++ apps/weblogger/src/java/org/apache/roller/weblogger/pojos/wrapper/WeblogBookmarkFolderWrapper.java
@@ -99,25 +99,8 @@ public class WeblogBookmarkFolderWrapper {
         return wrappedCollection;
     }
     
-    
+        
     public List getBookmarks() {
-        Set initialCollection = this.pojo.getBookmarks();
-        
-        // iterate through and wrap
-        // we force the use of an ArrayList because it should be good enough to cover
-        // for any Collection type we encounter.
-        ArrayList wrappedCollection = new ArrayList(initialCollection.size());
-        Iterator it = initialCollection.iterator();
-        int i = 0;
-        while(it.hasNext()) {
-            wrappedCollection.add(i,WeblogBookmarkWrapper.wrap((WeblogBookmark) it.next()));
-            i++;
-        }
-        
-        return wrappedCollection;
-    }
-    
-    public List getBookmarksSorted() {
         TreeSet initialCollection = new TreeSet(new BookmarkComparator());
         initialCollection.addAll(this.pojo.getBookmarks());
         
@@ -135,6 +118,7 @@ public class WeblogBookmarkFolderWrapper {
         return wrappedCollection;
     }    
         
+    
     public List retrieveBookmarks(boolean subfolders)
             throws WebloggerException {
         
