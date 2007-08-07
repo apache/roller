bcb288ea79c95db022c30b211fe68c57d44fe144
David M. Johnson <snoopdave@apache.org>
2007-08-07 19:15:30 +0000

    Fix for http://opensource.atlassian.com/projects/roller/browse/ROL-1497
    Give installation pages their own tile so they can set appropriate page title.
    
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@563627 13f79535-47bb-0310-9956-ffa450edef68

--- apps/weblogger/src/java/org/apache/roller/weblogger/ui/struts2/core/Install.java
+++ apps/weblogger/src/java/org/apache/roller/weblogger/ui/struts2/core/Install.java
@@ -67,11 +67,11 @@ public class Install extends UIAction {
     
     public String execute() {
         
-        if(WebloggerFactory.isBootstrapped()) {
+        if (WebloggerFactory.isBootstrapped()) {
             return SUCCESS;
         }
         
-        if(WebloggerStartup.getDatabaseProviderException() != null) {
+        if (WebloggerStartup.getDatabaseProviderException() != null) {
             StartupException se = WebloggerStartup.getDatabaseProviderException();
             if (se.getRootCause() != null) {
                 rootCauseException = se.getRootCause();
@@ -81,18 +81,21 @@ public class Install extends UIAction {
             messages = se.getStartupLog();
             
             log.debug("Forwarding to database error page");
+            setPageTitle("installer.error.connection.pageTitle");
             return DATABASE_ERROR;
         }   
         
         if (WebloggerStartup.isDatabaseCreationRequired()) {
             log.debug("Forwarding to database table creation page");
+            setPageTitle("installer.database.creation.pageTitle");
             return CREATE_DATABASE;
         }
         if (WebloggerStartup.isDatabaseUpgradeRequired()) {
             log.debug("Forwarding to database table upgrade page");
+            setPageTitle("installer.database.upgrade.pageTitle");
             return UPGRADE_DATABASE;
         }
-        
+        setPageTitle("installer.error.unknown.pageTitle");        
         return BOOTSTRAP;
     }
     
@@ -112,6 +115,7 @@ public class Install extends UIAction {
             messages = se.getStartupLog();
         }
         
+        setPageTitle("installer.database.creation.pageTitle");
         return CREATE_DATABASE;
     }
     
@@ -131,6 +135,7 @@ public class Install extends UIAction {
             messages = se.getStartupLog();
         }
         
+        setPageTitle("installer.database.upgrade.pageTitle");
         return UPGRADE_DATABASE;
     }
     
@@ -181,6 +186,7 @@ public class Install extends UIAction {
             rootCauseException = ex;
         }
         
+        setPageTitle("installer.error.unknown.pageTitle");                
         return BOOTSTRAP;
     }
     
