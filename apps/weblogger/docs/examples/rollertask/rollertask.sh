7de9ba1733c8ce9971ceb13da9aeae7a85ec04b9
David M. Johnson <snoopdave@apache.org>
2008-11-03 13:12:19 +0000

    Fixes to Roller Task example related to https://issues.apache.org/roller/browse/ROL-1758
    git-svn-id: https://svn.apache.org/repos/asf/roller/branches/roller_4.0@710051 13f79535-47bb-0310-9956-ffa450edef68

--- apps/weblogger/docs/examples/rollertask/rollertask.sh
+++ apps/weblogger/docs/examples/rollertask/rollertask.sh
@@ -13,32 +13,38 @@ fi
 # -- YOU MUST ENSURE THESE SETTINGS ARE CORRECT ---
 
 # Path to Java JDK
-JAVA_HOME=/usr/local/jdk1.5
+JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
 export JAVA_HOME
 
 # Directory of Roller context
-WEBAPP_DIR=/usr/local/tomcat/webapps/roller
+WEBAPP_DIR=${HOME}/src/roller_4.0/apps/weblogger/build/webapp
 export WEBAPP_DIR
 
-# Directory of additional jars
-JARS_DIR=/usr/local/tomcat/common/lib
+# Directory of additional jars (most importantly: your JDBC driver)
+JARS_DIR=${HOME}/tomcat/lib
 export JARS_DIR
 
-# Planet configuration override file specifying JDBC connection parameters
+# Roller configuration override file specifying JDBC connection parameters
 CONFIG_OVERRIDE=roller-custom.properties
 export CONFIG_OVERRIDE
+
+# Planet configuration override file specifying JDBC connection parameters
+PLANETCONFIG_OVERRIDE=planet-custom.properties
+export PLANETCONFIG_OVERRIDE
 
 
 # --- YOU SHOULD NOT NEED TO EDIT BELOW THIS LINE ---
 
+
+ROLLERTASK_CLASSPATH=${WEBAPP_DIR}/WEB-INF/lib/roller-business.jar
+
 # Hack: setting catalina.base=. allows us to save log in ./logs
+mkdirs ./logs
 
 ${JAVA_HOME}/bin/java \
     -Dcatalina.base=. \
     -Droller.custom.config=${CONFIG_OVERRIDE} \
-    -cp ${WEBAPP_DIR}/WEB-INF/lib/roller-business.jar \
-    ${WEBAPP_DIR}/WEB-INF/lib/roller-core.jar \
-    org.apache.roller.business.runnable.TaskRunner \
-    ${WEBAPP_DIR} ${JARS_DIR} \
-    $1
-
+    -Dplanet.custom.config=${PLANETCONFIG_OVERRIDE} \
+    -cp ${ROLLERTASK_CLASSPATH} \
+    org.apache.roller.weblogger.business.runnable.TaskRunner \
+    ${WEBAPP_DIR} ${JARS_DIR} $1
