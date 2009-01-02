0505eaba6940fa43064667c8539698cbba12e0fc
David M. Johnson <snoopdave@apache.org>
2009-01-02 15:18:19 +0000

    next up is RC2
    git-svn-id: https://svn.eu.apache.org/repos/asf/roller/branches/roller_4.0@730754 13f79535-47bb-0310-9956-ffa450edef68

--- apps/weblogger/sign-release.sh
+++ apps/weblogger/sign-release.sh
@@ -1,6 +1,6 @@
 
-export rcstring="-rc10"
-export vstring="4.0"
+export rcstring="-rc2"
+export vstring="4.0.1"
 
 cp dist/apache-roller-${vstring}.tar.gz dist/apache-roller-${vstring}${rcstring}.tar.gz
 cp dist/apache-roller-${vstring}.zip    dist/apache-roller-${vstring}${rcstring}.zip
