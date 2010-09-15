8a6e087699ca95f372a2d6419c6f1d15c2218db1
David M. Johnson <snoopdave@apache.org>
2010-09-15 01:57:54 +0000

    Preparing for RC2
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@997167 13f79535-47bb-0310-9956-ffa450edef68

--- sign-release.sh
+++ sign-release.sh
@@ -1,13 +1,8 @@
 export rcstring="-RC2"
 export vstring="5.0.0"
 
-#cp dist/roller-weblogger-${vstring}-binary.tar.gz dist/roller-weblogger-${vstring}${rcstring}-binary.tar.gz
-#cp dist/roller-weblogger-${vstring}-binary.zip    dist/roller-weblogger-${vstring}${rcstring}-binary.zip
-#cp dist/roller-weblogger-${vstring}-source.tar.gz dist/roller-weblogger-${vstring}${rcstring}-source.tar.gz
-#cp dist/roller-weblogger-${vstring}-source.zip    dist/roller-weblogger-${vstring}${rcstring}-source.zip
-
-gpg --armor --detach-sig dist/roller-weblogger-${vstring}${rcstring}-binary.tar.gz
-gpg --armor --detach-sig dist/roller-weblogger-${vstring}${rcstring}-binary.zip
-gpg --armor --detach-sig dist/roller-weblogger-${vstring}${rcstring}-source.tar.gz 
-gpg --armor --detach-sig dist/roller-weblogger-${vstring}${rcstring}-source.zip
+gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-binary.tar.gz
+gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-binary.zip
+gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.tar.gz 
+gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.zip
 
