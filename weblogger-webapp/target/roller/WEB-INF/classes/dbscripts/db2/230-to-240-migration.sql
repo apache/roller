81114ea563e875f1d062de9f26317b8982c52cc9
David M. Johnson <snoopdave@apache.org>
2011-01-30 19:00:51 +0000

    Removing target dir from source tree
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1065349 13f79535-47bb-0310-9956-ffa450edef68

--- weblogger-webapp/target/roller/WEB-INF/classes/dbscripts/db2/230-to-240-migration.sql
+++
@@ -1,7 +0,0 @@
-
-
--- Roller 2.4 schema changes
-
-    alter table pingtarget add column autoenabled smallint with default 0 not null;
-
-    alter table website add column lastmodified timestamp default null;
--- weblogger-webapp/target/roller/WEB-INF/classes/dbscripts/mssql/230-to-240-migration.sql
+++
@@ -1,6 +0,0 @@
-
-
--- Roller 2.4 schema changes
-
-
-    alter table website add column lastmodified datetime default null;
