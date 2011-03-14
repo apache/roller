3c4141989625bd669e020ef5d9414f1cc85c647d
Thiago Alvarenga Lechuga <thiagoalz@gmail.com>
2011-03-14 14:06:35 -0300

    removing empty spaces in xml and sh files

--- sign-tomcat-release.sh
+++ sign-tomcat-release.sh
@@ -3,6 +3,6 @@ export vstring="5.0.0"
 
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-tomcat.tar.gz
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-tomcat.zip
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.tar.gz 
+gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.tar.gz
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.zip
 
