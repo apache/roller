9642a21119ce7ac8f0134661fba4ea8869da73a3
Greg Huber <ghuber@apache.org>
2012-01-29 19:28:14 +0000

    Will not compile in eclipse.  Override not valid.
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1237423 13f79535-47bb-0310-9956-ffa450edef68

--- weblogger-business/src/main/java/org/apache/roller/weblogger/business/themes/SharedThemeTemplateCode.java
+++ weblogger-business/src/main/java/org/apache/roller/weblogger/business/themes/SharedThemeTemplateCode.java
@@ -1,20 +1,20 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ */
 
 package org.apache.roller.weblogger.business.themes;
 
@@ -28,96 +28,94 @@ import java.io.Serializable;
  */
 public class SharedThemeTemplateCode implements Serializable, TemplateCode {
 
-    private String templateId = null;
-    private String template = null;
-    private String type = null;
-    private String ContentType = null;
-    private String templateLanguage = null;
+	private String templateId = null;
+	private String template = null;
+	private String type = null;
+	private String ContentType = null;
+	private String templateLanguage = null;
 
-    public SharedThemeTemplateCode(String templateId, String type){
-        this.templateId = templateId;
-        this.type = type;
-    }
+	public SharedThemeTemplateCode(String templateId, String type) {
+		this.templateId = templateId;
+		this.type = type;
+	}
 
-    public SharedThemeTemplateCode() {
-    }
+	public SharedThemeTemplateCode() {
+	}
 
-	@Override
-    public String getTemplate() {
-        return template;
-    }
+	// @Override
+	public String getTemplate() {
+		return template;
+	}
 
-	@Override
-    public void setTemplate(String template) {
-        this.template = template;
-    }
+	// @Override
+	public void setTemplate(String template) {
+		this.template = template;
+	}
 
-	@Override
-    public String getTemplateId() {
-        return templateId;
-    }
+	// @Override
+	public String getTemplateId() {
+		return templateId;
+	}
 
-	@Override
-    public void setTemplateId(String templateId) {
-        this.templateId = templateId;
-    }
+	// @Override
+	public void setTemplateId(String templateId) {
+		this.templateId = templateId;
+	}
 
-	@Override
-    public String getType() {
-        return type;
-    }
+	// @Override
+	public String getType() {
+		return type;
+	}
 
-	@Override
-    public void setType(String type) {
-        this.type = type;
-    }
+	// @Override
+	public void setType(String type) {
+		this.type = type;
+	}
 
- //------------------------------------------------------- Good citizenship
+	// ------------------------------------------------------- Good citizenship
 
-    public String toString() {
-        StringBuffer buf = new StringBuffer();
-        buf.append("{");
-        buf.append(this.templateId);
-        buf.append(", [ ").append(this.template);
-        buf.append("] , ").append(this.type);
-        buf.append("}");
-        return buf.toString();
-    }
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("{");
+		buf.append(this.templateId);
+		buf.append(", [ ").append(this.template);
+		buf.append("] , ").append(this.type);
+		buf.append("}");
+		return buf.toString();
+	}
 
-    public boolean equals(Object other) {
-        if (other == this) return true;
-        if (other instanceof SharedThemeTemplateCode != true) return false;
-        SharedThemeTemplateCode o = (SharedThemeTemplateCode)other;
-        return new EqualsBuilder()
-            .append(templateId, o.getTemplateId())
-            .append(template, o.getTemplate())
-            .isEquals();
-    }
+	public boolean equals(Object other) {
+		if (other == this)
+			return true;
+		if (other instanceof SharedThemeTemplateCode != true)
+			return false;
+		SharedThemeTemplateCode o = (SharedThemeTemplateCode) other;
+		return new EqualsBuilder().append(templateId, o.getTemplateId())
+				.append(template, o.getTemplate()).isEquals();
+	}
 
-    public int hashCode() {
-        return new HashCodeBuilder()
-            .append(getTemplateId())
-            .append(getTemplate())
-            .toHashCode();
-    }
+	public int hashCode() {
+		return new HashCodeBuilder().append(getTemplateId())
+				.append(getTemplate()).toHashCode();
+	}
 
-	@Override
-    public String getTemplateLanguage() {
-        return templateLanguage;
-    }
+	// @Override
+	public String getTemplateLanguage() {
+		return templateLanguage;
+	}
 
-	@Override
-    public void setTemplateLanguage(String templateLanguage) {
-        this.templateLanguage = templateLanguage;
-    }
+	// @Override
+	public void setTemplateLanguage(String templateLanguage) {
+		this.templateLanguage = templateLanguage;
+	}
 
-	@Override
-    public String getContentType() {
-        return ContentType;
-    }
+	// @Override
+	public String getContentType() {
+		return ContentType;
+	}
 
-	@Override
-    public void setContentType(String contentType) {
-        ContentType = contentType;
-    }
+	// @Override
+	public void setContentType(String contentType) {
+		ContentType = contentType;
+	}
 }
--- weblogger-business/src/main/java/org/apache/roller/weblogger/pojos/WeblogThemeTemplateCode.java
+++ weblogger-business/src/main/java/org/apache/roller/weblogger/pojos/WeblogThemeTemplateCode.java
@@ -1,20 +1,20 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ */
 
 package org.apache.roller.weblogger.pojos;
 
@@ -38,129 +38,123 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "rol_templatecode")
 @NamedQueries({
-@NamedQuery(name =            "WeblogThemeTemplateCode.getTemplateCodeByType",
-        query = "SELECT c FROM WeblogThemeTemplateCode c WHERE c.templateId = ?1 AND c.type =?2"),
+		@NamedQuery(name = "WeblogThemeTemplateCode.getTemplateCodeByType", query = "SELECT c FROM WeblogThemeTemplateCode c WHERE c.templateId = ?1 AND c.type =?2"),
 
-     @NamedQuery(name =       "WeblogThemeTemplateCode.getTemplateCodesByTemplateId",
-        query = "SELECT c FROM WeblogThemeTemplateCode c WHERE c.templateId = ?1 ")
-})
+		@NamedQuery(name = "WeblogThemeTemplateCode.getTemplateCodesByTemplateId", query = "SELECT c FROM WeblogThemeTemplateCode c WHERE c.templateId = ?1 ") })
 public class WeblogThemeTemplateCode implements Serializable, TemplateCode {
 
+	private static final long serialVersionUID = -1497618963802805151L;
+	private String id = UUIDGenerator.generateUUID();
+	private String templateId = null;
+	// template contents
+	private String template = null;
+	private String type = null;
+	private String ContentType = null;
+	private String templateLanguage = null;
 
-    private static final long serialVersionUID = -1497618963802805151L;
-    private String id = UUIDGenerator.generateUUID();
-    private String templateId = null;
-    //template contents
-    private String template = null;
-    private String type = null;
-    private String ContentType = null;
-    private String templateLanguage = null;
+	public WeblogThemeTemplateCode(String templateId, String type) {
+		this.templateId = templateId;
+		this.type = type;
+	}
 
-    public WeblogThemeTemplateCode(String templateId, String type){
-        this.templateId = templateId;
-        this.type = type;
-    }
+	public WeblogThemeTemplateCode() {
+	}
 
-    public WeblogThemeTemplateCode() {
-    }
+	@Id
+	@Column(nullable = false, updatable = false)
+	public String getId() {
+		return id;
+	}
 
-    @Id
-    @Column(nullable = false, updatable = false)
-    public String getId() {
-        return id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	@Basic
+	@Column(nullable = false, updatable = true, insertable = true)
+	// @Override
+	public String getTemplate() {
+		return template;
+	}
 
-    @Basic
-    @Column(nullable = false, updatable = true, insertable = true)
-	@Override
-    public String getTemplate() {
-        return template;
-    }
+	// @Override
+	public void setTemplate(String template) {
+		this.template = template;
+	}
 
-	@Override
-    public void setTemplate(String template) {
-        this.template = template;
-    }
+	@Basic
+	@Column(nullable = false, updatable = true, insertable = true)
+	// @Override
+	public String getTemplateId() {
+		return templateId;
+	}
 
-    @Basic
-    @Column(nullable = false, updatable = true, insertable = true)
-	@Override
-    public String getTemplateId() {
-        return templateId;
-    }
+	// @Override
+	public void setTemplateId(String templateId) {
+		this.templateId = templateId;
+	}
 
-	@Override
-    public void setTemplateId(String templateId) {
-        this.templateId = templateId;
-    }
+	@Basic
+	@Column(nullable = false, updatable = true, insertable = true)
+	// @Override
+	public String getType() {
+		return type;
+	}
 
-    @Basic
-    @Column(nullable = false, updatable = true, insertable = true)
-	@Override
-    public String getType() {
-        return type;
-    }
+	// @Override
+	public void setType(String type) {
+		this.type = type;
+	}
 
-	@Override
-    public void setType(String type) {
-        this.type = type;
-    }
+	// ------------------------------------------------------- Good citizenship
 
- //------------------------------------------------------- Good citizenship
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("{");
+		buf.append(this.id);
+		buf.append(", ").append(this.templateId);
+		buf.append(", [ ").append(this.template);
+		buf.append("] , ").append(this.type);
+		buf.append("}");
+		return buf.toString();
+	}
 
-    public String toString() {
-        StringBuffer buf = new StringBuffer();
-        buf.append("{");
-        buf.append(this.id);
-        buf.append(", ").append(this.templateId);
-        buf.append(", [ ").append(this.template);
-        buf.append("] , ").append(this.type);
-        buf.append("}");
-        return buf.toString();
-    }
+	public boolean equals(Object other) {
+		if (other == this)
+			return true;
+		if (other instanceof WeblogThemeTemplateCode != true)
+			return false;
+		WeblogThemeTemplateCode o = (WeblogThemeTemplateCode) other;
+		return new EqualsBuilder().append(templateId, o.getTemplateId())
+				.append(template, o.getTemplate()).isEquals();
+	}
 
-    public boolean equals(Object other) {
-        if (other == this) return true;
-        if (other instanceof WeblogThemeTemplateCode != true) return false;
-        WeblogThemeTemplateCode o = (WeblogThemeTemplateCode)other;
-        return new EqualsBuilder()
-            .append(templateId, o.getTemplateId())
-            .append(template, o.getTemplate())
-            .isEquals();
-    }
+	public int hashCode() {
+		return new HashCodeBuilder().append(getTemplateId())
+				.append(getTemplate()).toHashCode();
+	}
 
-    public int hashCode() {
-        return new HashCodeBuilder()
-            .append(getTemplateId())
-            .append(getTemplate())
-            .toHashCode();
-    }
+	@Basic
+	@Column(name = "templatelang", unique = false, updatable = true, insertable = true)
+	// @Override
+	public String getTemplateLanguage() {
+		return templateLanguage;
+	}
 
-     @Basic
-    @Column(name ="templatelang",unique = false, updatable = true, insertable = true)
-	@Override
-    public String getTemplateLanguage() {
-        return templateLanguage;
-    }
+	// @Override
+	public void setTemplateLanguage(String templateLanguage) {
+		this.templateLanguage = templateLanguage;
+	}
 
-	@Override
-    public void setTemplateLanguage(String templateLanguage) {
-        this.templateLanguage = templateLanguage;
-    }
+	@Basic
+	@Column(name = "contenttype", unique = false, updatable = true, insertable = true)
+	// @Override
+	public String getContentType() {
+		return ContentType;
+	}
 
-     @Basic
-    @Column(name= "contenttype", unique = false, updatable = true, insertable = true)
-	@Override
-    public String getContentType() {
-        return ContentType;
-    }
-
-	@Override
-    public void setContentType(String contentType) {
-        ContentType = contentType;
-    }
+	// @Override
+	public void setContentType(String contentType) {
+		ContentType = contentType;
+	}
 }
