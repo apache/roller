541e2329a305382a482688e58694d217611da7fe
David M. Johnson <snoopdave@apache.org>
2012-02-12 14:08:46 +0000

    Commiting fix from Tiger Gui to improve the standard to mobile page switch button and logic: https://issues.apache.org/jira/browse/ROL-1937
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1243255 13f79535-47bb-0310-9956-ffa450edef68

--- weblogger-web/src/main/java/org/apache/roller/weblogger/ui/rendering/mobile/MobileDeviceRepository.java
+++ weblogger-web/src/main/java/org/apache/roller/weblogger/ui/rendering/mobile/MobileDeviceRepository.java
@@ -49,6 +49,7 @@ public class MobileDeviceRepository {
 			+ "vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\\\\-|2|g)|"
 			+ "yas\\\\-|your|zeto|zte\\\\-";
 	public static final String USER_REQUEST_TYPE = "roller_user_request_type";
+	public static final String USER_AGENT_PARAMETER = "deviceType";
 
 	/**
 	 *
@@ -65,12 +66,17 @@ public class MobileDeviceRepository {
 
 	public static DeviceType getRequestType(HttpServletRequest request) {
 		DeviceType type = DeviceType.standard;
+		
+		String deviceTypeParam = request.getParameter(USER_AGENT_PARAMETER);
+		if (deviceTypeParam !=null){
+			return deviceTypeParam.trim().equals("standard") ? DeviceType.standard : DeviceType.mobile;
+		}
+		
 		String cookie = getCookieValue(request.getCookies(), USER_REQUEST_TYPE, null);
-
 		if (cookie != null) {
 			return cookie.equals("standard") ? DeviceType.standard : DeviceType.mobile;
 		}
-
+		
 		if (isMobileDevice(request)) {
 			type = DeviceType.mobile; 
 		}
