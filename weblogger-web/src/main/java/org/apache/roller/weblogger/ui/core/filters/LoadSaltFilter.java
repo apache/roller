ca6ea195329b9675f6952db0567f09b7bb177835
David M. Johnson <snoopdave@apache.org>
2012-05-06 21:52:59 +0000

    Beginnings of a Salt filter
    git-svn-id: https://svn.apache.org/repos/asf/roller/branches/roller_5.0@1334797 13f79535-47bb-0310-9956-ffa450edef68

---
+++ weblogger-web/src/main/java/org/apache/roller/weblogger/ui/core/filters/LoadSaltFilter.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ */
+
+package org.apache.roller.weblogger.ui.core.filters;
+ 
+import java.io.IOException;
+import java.security.SecureRandom;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.roller.weblogger.util.LRUCache2;
+ 
+public class LoadSaltFilter implements Filter {
+ 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) request;
+
+		LRUCache2 saltCache = (LRUCache2)
+            httpReq.getSession().getAttribute("saltCache");
+        if (saltCache == null){
+            saltCache = new LRUCache2(10000,3000); 
+            httpReq.getSession().setAttribute("saltCache", saltCache);
+        }
+        String salt = RandomStringUtils.random(20, 0, 0, true, true, null, new SecureRandom());
+        saltCache.put(salt, Boolean.TRUE);
+        httpReq.setAttribute("salt", salt);
+
+        chain.doFilter(request, response);
+    }
+ 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+ 
+    @Override
+    public void destroy() {
+    }
+}
---
+++ weblogger-web/src/main/java/org/apache/roller/weblogger/ui/core/filters/ValidateSaltFilter.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ */
+
+package org.apache.roller.weblogger.ui.core.filters;
+ 
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.roller.weblogger.util.LRUCache2;
+ 
+public class ValidateSaltFilter implements Filter  {
+ 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) request;
+ 
+        String salt = (String) httpReq.getParameter("salt");
+		LRUCache2 saltCache = (LRUCache2)
+            httpReq.getSession().getAttribute("saltCache");
+
+        if (saltCache != null && salt != null && saltCache.get(salt) != null){
+            chain.doFilter(request, response);
+        } else {
+            throw new ServletException("Security Violation");
+        }
+    }
+ 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+ 
+    @Override
+    public void destroy() {
+    }
+}
