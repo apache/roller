62fa6a2b24f5c06b0750864a9c38f1f2d390efec
Glen Mazza <gmazza@apache.org>
2013-08-03 00:25:43 +0000

    Removed no longer needed sign-release.sh (it's in the assembly-release pom already) and simplified the assembly release configuration
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1509918 13f79535-47bb-0310-9956-ffa450edef68

--- sign-release.sh
+++
@@ -1,7 +0,0 @@
-export rcstring=""
-export vstring="5.1"
-
-gpg --armor --detach-sig assembly-release/target/roller-weblogger-${vstring}${rcstring}.tar.gz
-gpg --armor --detach-sig assembly-release/target/roller-weblogger-${vstring}${rcstring}.zip
-gpg --armor --detach-sig assembly-release/target/roller-weblogger-${vstring}${rcstring}-source.tar.gz 
-gpg --armor --detach-sig assembly-release/target/roller-weblogger-${vstring}${rcstring}-source.zip
