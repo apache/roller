02f39c9559f56d8e94d4127ad2ef326d506c512f
Glen Mazza <gmazza@apache.org>
2013-08-18 18:08:58 +0000

    ROL-1596 fix off-by-one error in display of # of weblog entries.
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1515190 13f79535-47bb-0310-9956-ffa450edef68

--- app/src/main/java/org/apache/roller/weblogger/ui/core/security/CustomUserRegistry.java
+++ app/src/main/java/org/apache/roller/weblogger/ui/core/security/CustomUserRegistry.java
@@ -29,7 +29,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.ldap.userdetails.LdapUserDetails;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.roller.weblogger.config.WebloggerConfig;
