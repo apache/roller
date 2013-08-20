98259364e39f9d5026e16a9f4da7d79b06d964df
David M. Johnson <snoopdave@apache.org>
2013-08-20 20:47:51 +0000

    preparing for 5.0.2
    git-svn-id: https://svn.apache.org/repos/asf/roller/branches/roller_5.0@1515964 13f79535-47bb-0310-9956-ffa450edef68

--- sign-javaee-release.sh
+++ sign-javaee-release.sh
@@ -1,5 +1,5 @@
 export rcstring=""
-export vstring="5.0.1"
+export vstring="5.0.2"
 
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-javaee.tar.gz
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-javaee.zip
--- sign-jboss-release.sh
+++ sign-jboss-release.sh
@@ -1,5 +1,5 @@
 export rcstring=""
-export vstring="5.0.0"
+export vstring="5.0.2"
 
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-jboss.tar.gz
 gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-jboss.zip
