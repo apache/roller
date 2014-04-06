7d3b7f3049eec5ba1ccd0d2793b0f7fa819cd443
Glen Mazza <gmazza@apache.org>
2014-04-06 03:25:28 +0000

    Better consolidation of Jakarta taglib code that Roller is sitll using.
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1585239 13f79535-47bb-0310-9956-ffa450edef68

--- app/src/main/java/org/apache/roller/weblogger/ui/tags/TruncateNicelyTag.java
+++ app/src/main/java/org/apache/roller/weblogger/ui/tags/TruncateNicelyTag.java
@@ -19,6 +19,8 @@ import javax.servlet.jsp.JspException;
 import org.apache.commons.lang3.math.NumberUtils;
 
 /**
+ * Code grabbed from retired Jakarta Tablibs project: http://jakarta.apache.org/taglibs/string/
+ *
  * A more intelligent substring.  It attempts to cut off a string after
  * a space, following predefined or user-supplied lower and upper limits,
  * useful for making short descriptions from long text.  Can also strip
@@ -38,15 +40,14 @@ import org.apache.commons.lang3.math.NumberUtils;
  *             String to append to end of truncated string.
  * </dd>
  * </dl>
- * 
+ *
  * @author timster@mac.com
  */
 public class TruncateNicelyTag extends StringTagSupport {
 
-	private String stripMarkup;
     private String lower;
     private String upper;
-	private String appendToEnd;
+    private String appendToEnd;
 
     public TruncateNicelyTag() {
         super();
@@ -54,7 +55,8 @@ public class TruncateNicelyTag extends StringTagSupport {
 
     /**
      * Get the lower property
-	 * @return String lower property
+     *
+     * @return String lower property
      */
     public String getLower() {
         return this.lower;
@@ -62,15 +64,17 @@ public class TruncateNicelyTag extends StringTagSupport {
 
     /**
      * Set the upper property
+     *
      * @param lower String property
      */
-    public void setLower(String l) {
-        this.lower = l;
+    public void setLower(String lower) {
+        this.lower = lower;
     }
 
     /**
      * Get the upper property
-	 * @return String upper property
+     *
+     * @return String upper property
      */
     public String getUpper() {
         return this.upper;
@@ -78,34 +82,114 @@ public class TruncateNicelyTag extends StringTagSupport {
 
     /**
      * Set the upper property
+     *
      * @param upper String property
      */
-    public void setUpper(String u) {
-        this.upper = u;
+    public void setUpper(String upper) {
+        this.upper = upper;
     }
 
-	public String getAppendToEnd() {
-		return this.appendToEnd;
-	}
+    public String getAppendToEnd() {
+        return this.appendToEnd;
+    }
 
-	public void setAppendToEnd(String s) {
-		this.appendToEnd = s;
-	}
+    public void setAppendToEnd(String s) {
+        this.appendToEnd = s;
+    }
 
     public String changeString(String text) throws JspException {
-					
-		int l = NumberUtils.toInt(lower);
-		int u = NumberUtils.toInt(upper);
-	
-		return StringW.truncateNicely(text, l, u, this.appendToEnd);
+
+        int l = NumberUtils.toInt(lower);
+        int u = NumberUtils.toInt(upper);
+
+        return truncateNicely(text, l, u, this.appendToEnd);
     }
 
     public void initAttributes() {
 
-		this.lower = "10";
-		this.upper = "-1";
-		this.appendToEnd = "...";
+        this.lower = "10";
+        this.upper = "-1";
+        this.appendToEnd = "...";
 
+    }
+
+    /**
+     * Truncates a string nicely. It will search for the first space
+     * after the lower limit and truncate the string there.  It will
+     * also append any string passed as a parameter to the end of the
+     * string.  The hard limit can be specified to forcibily truncate a
+     * string (in the case of an extremely long word or such).  All
+     * HTML/XML markup will be stripped from the string prior to
+     * processing for truncation.
+     *
+     * @param str         String the string to be truncated.
+     * @param lower       int value of the lower limit.
+     * @param upper       int value of the upper limit, -1 if no limit is
+     *                    desired. If the uppper limit is lower than the
+     *                    lower limit, it will be adjusted to be same as
+     *                    the lower limit.
+     * @param appendToEnd String to be appended to the end of the
+     *                    truncated string.
+     *                    This is appended ONLY if the string was indeed
+     *                    truncated. The append is does not count towards
+     *                    any lower/upper limits.
+     */
+    private static String truncateNicely(String str, int lower, int upper, String appendToEnd)
+    {
+        // strip markup from the string
+        str = removeXml(str);
+
+        // quickly adjust the upper if it is set lower than 'lower'
+        if(upper < lower) {
+            upper = lower;
+        }
+
+        // now determine if the string fits within the upper limit
+        // if it does, go straight to return, do not pass 'go' and collect $200
+        if(str.length() > upper) {
+            // the magic location int
+            int loc;
+
+            // first we determine where the next space appears after lower
+            loc = str.lastIndexOf(' ', upper);
+
+            // now we'll see if the location is greater than the lower limit
+            if(loc >= lower) {
+                // yes it was, so we'll cut it off here
+                str = str.substring(0, loc);
+            } else {
+                // no it wasn't, so we'll cut it off at the upper limit
+                str = str.substring(0, upper);
+            }
+
+            // the string was truncated, so we append the appendToEnd String
+            str = str + appendToEnd;
+        }
+
+        return str;
+    }
+
+    /**
+     * Remove any xml tags from a String.
+     */
+    private static String removeXml(String str) {
+        int sz = str.length();
+        StringBuilder buffer = new StringBuilder(sz);
+        boolean inTag = false;
+        for (int i=0; i<sz; i++) {
+            char ch = str.charAt(i);
+            if (ch == '<') {
+                inTag = true;
+            } else
+            if (ch == '>') {
+                inTag = false;
+                continue;
+            }
+            if (!inTag) {
+                buffer.append(ch);
+            }
+        }
+        return buffer.toString();
     }
 
 }
