e363ca54f0877d8691388eb1534c2c74df3063a1
Glen Mazza <gmazza@apache.org>
2014-08-20 18:23:38 +0000

    Added utility for signing files, until we've updated our POM to handle this.
    git-svn-id: https://svn.apache.org/repos/asf/roller/trunk@1619195 13f79535-47bb-0310-9956-ffa450edef68

---
+++ assembly-release/sign-release.sh
@@ -0,0 +1,8 @@
+export rcstring=""
+export vstring="5.1.0"
+
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.tar.gz
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.zip
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-source.tar.gz 
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-source.zip
+
