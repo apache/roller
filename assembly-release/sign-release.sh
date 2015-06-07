4c40896834e15db8582d2ea890470763b9a4dae2
Glen Mazza <glen.mazza@gmail.com>
2015-06-07 19:36:52 -0400

    Issue #39 remove assembly-release folder

--- assembly-release/sign-release.sh
+++
@@ -1,8 +0,0 @@
-export rcstring=""
-export vstring="5.2.0"
-
-gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.tar.gz
-gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.zip
-gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-source.tar.gz 
-gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-source.zip
-
