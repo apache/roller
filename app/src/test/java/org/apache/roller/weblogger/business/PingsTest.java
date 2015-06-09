8d237c408cde6d4cb5cc760f254da2f948716a95
Glen Mazza <glen.mazza@gmail.com>
2015-06-08 20:53:41 -0400

    Continued work on issue 33.

--- app/src/test/java/org/apache/roller/weblogger/business/PingsTest.java
+++ app/src/test/java/org/apache/roller/weblogger/business/PingsTest.java
@@ -1,26 +1,23 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  The ASF licenses this file to You
-* under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.  For additional information regarding
-* copyright in this work, please see the NOTICE file in the top level
-* directory of this distribution.
-*/
-/*
- * PingsTest.java
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Created on April 9, 2006, 1:27 PM
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  For additional information regarding
+ * copyright in this work, please see the NOTICE file in the top level
+ * directory of this distribution.
+ *
+ * Source file modified from the original ASF source; all changes made
+ * are also under Apache License.
  */
-
 package org.apache.roller.weblogger.business;
 
 import java.util.ArrayList;
@@ -165,8 +162,8 @@ public class PingsTest extends TestCase {
         // lookup all common pings
         List commonPings = mgr.getCommonPingTargets();
         assertNotNull(commonPings);
-        // correct answer is: 4 pings in config + 1 new one = 5
-        assertEquals(5, commonPings.size());
+        // correct answer is: 3 pings in config + 1 new one = 5
+        assertEquals(4, commonPings.size());
         
         // delete common ping
         ping = null;
