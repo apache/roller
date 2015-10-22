7e4f6fe60312339769e53f1fd00c2456418c0e26
Glen Mazza <glen.mazza@gmail.com>
2015-10-21 21:59:46 -0400

    Issue 219: Remove media file search.

--- app/src/test/java/org/apache/roller/weblogger/business/MediaFileTest.java
+++ app/src/test/java/org/apache/roller/weblogger/business/MediaFileTest.java
@@ -21,8 +21,6 @@
 package org.apache.roller.weblogger.business;
 
 import java.io.File;
-import java.sql.Timestamp;
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -36,15 +34,10 @@ import org.apache.roller.weblogger.WebloggerException;
 import org.apache.roller.weblogger.config.WebloggerConfig;
 import org.apache.roller.weblogger.pojos.MediaFile;
 import org.apache.roller.weblogger.pojos.MediaFileDirectory;
-import org.apache.roller.weblogger.pojos.MediaFileFilter;
-import org.apache.roller.weblogger.pojos.MediaFileFilter.MediaFileOrder;
-import org.apache.roller.weblogger.pojos.MediaFileFilter.SizeFilterType;
-import org.apache.roller.weblogger.pojos.MediaFileType;
 import org.apache.roller.weblogger.pojos.User;
 import org.apache.roller.weblogger.pojos.Weblog;
 import org.apache.roller.weblogger.util.RollerMessages;
 import org.junit.FixMethodOrder;
-import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 /**
@@ -72,7 +65,6 @@ public class MediaFileTest extends TestCase {
     /**
      * Test creation of directory by path
      */
-    @Test
     public void testCreateMediaFileDirectoryByPath() throws Exception {
         User testUser;
         Weblog testWeblog;
@@ -152,7 +144,6 @@ public class MediaFileTest extends TestCase {
     /**
      * Test directory creation
      */
-    @Test
     public void testCreateMediaFileDirectory() throws Exception {
         User testUser;
         Weblog testWeblog;
@@ -371,343 +362,6 @@ public class MediaFileTest extends TestCase {
         assertEquals("test.jpg", mediaFile1.getName());
         assertEquals("This is a test image", mediaFile1.getNotes());
         assertEquals(2000, mediaFile1.getLength());
-
-        TestUtils.endSession(true);
-        TestUtils.teardownWeblog(testWeblog.getId());
-        TestUtils.teardownUser(testUser.getUserName());
-    }
-
-    /**
-     * Test searching media file.
-     */
-    public void testSearchMediaFile() throws Exception {
-        User testUser;
-        Weblog testWeblog;
-        testUser = TestUtils.setupUser("mediaFileTestUser7");
-        testWeblog = TestUtils.setupWeblog("mediaFileTestWeblog7", testUser);
-
-        MediaFileManager mfMgr = WebloggerFactory.getWeblogger()
-                .getMediaFileManager();
-
-        testWeblog = TestUtils.getManagedWebsite(testWeblog);
-        MediaFileDirectory rootDirectory = mfMgr
-                .getDefaultMediaFileDirectory(testWeblog);
-
-        try {
-
-            String id1;
-            {
-                MediaFile mf = new MediaFile();
-                mf.setName("test_work.jpg");
-                mf.setNotes("This is a test image");
-                mf.setLength(2000);
-                mf.setDirectory(rootDirectory);
-                mf.setInputStream(getClass().getResourceAsStream(TEST_IMAGE));
-                mf.setContentType("image/jpeg");
-                rootDirectory.getMediaFiles().add(mf);
-
-                mfMgr.createMediaFile(testWeblog, mf, new RollerMessages());
-
-                TestUtils.endSession(true);
-                id1 = mf.getId();
-                assertNotNull(mf.getId());
-                assertNotNull(mf.getId().length() > 0);
-            }
-
-            String id2;
-            {
-                testWeblog = TestUtils.getManagedWebsite(testWeblog);
-                rootDirectory = mfMgr.getMediaFileDirectory(rootDirectory
-                        .getId());
-
-                MediaFile mf = new MediaFile();
-                mf.setName("test_home.jpg");
-                mf.setNotes("This is a test image");
-                mf.setLength(3000);
-                mf.setDirectory(rootDirectory);
-                mf.setInputStream(getClass().getResourceAsStream(TEST_IMAGE));
-                mf.setContentType("image/jpeg");
-                rootDirectory.getMediaFiles().add(mf);
-
-                mfMgr.createMediaFile(testWeblog, mf, new RollerMessages());
-
-                TestUtils.endSession(true);
-                id2 = mf.getId();
-                assertNotNull(mf.getId());
-                assertNotNull(mf.getId().length() > 0);
-            }
-
-            String id3 = null;
-            {
-                testWeblog = TestUtils.getManagedWebsite(testWeblog);
-                rootDirectory = mfMgr.getMediaFileDirectory(rootDirectory
-                        .getId());
-
-                MediaFile mf = new MediaFile();
-                mf.setName("test_pers.jpg");
-                mf.setNotes("This is a personal test image");
-                mf.setLength(4000);
-                mf.setDirectory(rootDirectory);
-                mf.setInputStream(getClass().getResourceAsStream(TEST_IMAGE));
-                mf.setContentType("image/jpeg");
-                rootDirectory.getMediaFiles().add(mf);
-
-                mfMgr.createMediaFile(testWeblog, mf, new RollerMessages());
-
-                TestUtils.endSession(true);
-                id3 = mf.getId();
-                assertNotNull(mf.getId());
-                assertNotNull(mf.getId().length() > 0);
-            }
-
-            testWeblog = TestUtils.getManagedWebsite(testWeblog);
-
-            List<MediaFile> searchResults;
-
-            // search by name
-
-            MediaFileFilter filter1 = new MediaFileFilter();
-            filter1.setName("mytest.jpg");
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter1);
-            assertTrue(searchResults.isEmpty());
-
-            MediaFileFilter filter2 = new MediaFileFilter();
-            filter2.setName("test_home.jpg");
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter2);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(id2, (searchResults.get(0)).getId());
-            assertNotNull((searchResults.get(0)).getDirectory());
-            assertEquals("default", (searchResults.get(0)).getDirectory()
-                    .getName());
-
-            MediaFileFilter filter3 = new MediaFileFilter();
-            filter3.setName("test_work.jpg");
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter3);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(id1, (searchResults.get(0)).getId());
-
-            // search by size
-            MediaFileFilter filter6 = new MediaFileFilter();
-            filter6.setSize(3000);
-            filter6.setSizeFilterType(MediaFileFilter.SizeFilterType.LT);
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter6);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(1, searchResults.size());
-            assertEquals("test_work.jpg",
-                    (searchResults.get(0)).getName());
-
-            MediaFileFilter filter7 = new MediaFileFilter();
-            filter7.setSize(3000);
-            filter7.setSizeFilterType(MediaFileFilter.SizeFilterType.EQ);
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter7);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(1, searchResults.size());
-            assertEquals("test_home.jpg",
-                    (searchResults.get(0)).getName());
-
-            MediaFileFilter filter8 = new MediaFileFilter();
-            filter8.setSize(3000);
-            filter8.setSizeFilterType(MediaFileFilter.SizeFilterType.GT);
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter8);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(1, searchResults.size());
-            assertEquals("test_pers.jpg",
-                    (searchResults.get(0)).getName());
-
-            MediaFileFilter filter9 = new MediaFileFilter();
-            filter9.setSize(3000);
-            filter9.setSizeFilterType(MediaFileFilter.SizeFilterType.LTE);
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter9);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(2, searchResults.size());
-
-            MediaFileFilter filter10 = new MediaFileFilter();
-            filter10.setSize(3000);
-            filter10.setSizeFilterType(MediaFileFilter.SizeFilterType.GTE);
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter10);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(2, searchResults.size());
-
-            // search by type
-            MediaFileFilter filter11 = new MediaFileFilter();
-            filter11.setType(MediaFileType.IMAGE);
-            searchResults = mfMgr.searchMediaFiles(testWeblog, filter11);
-            assertFalse(searchResults.isEmpty());
-            assertEquals(3, searchResults.size());
-
-        } finally {
-            TestUtils.endSession(true);
-            TestUtils.teardownWeblog(testWeblog.getId());
-            TestUtils.teardownUser(testUser.getUserName());
-        }
-
-    }
-
-    /**
-     * Test searching media file with paging logic.
-     */
-    public void testSearchMediaFilePaging() throws Exception {
-        User testUser;
-        Weblog testWeblog;
-        testUser = TestUtils.setupUser("mediaFileTestUser9");
-        testWeblog = TestUtils.setupWeblog("mediaFileTestWeblog9", testUser);
-
-        try {
-            MediaFileManager mfMgr = WebloggerFactory.getWeblogger()
-                    .getMediaFileManager();
-
-            // no need to create root directory, that is done automatically now
-            MediaFileDirectory rootDirectory = mfMgr
-                    .getDefaultMediaFileDirectory(testWeblog);
-
-            for (int i = 0; i < 15; i++) {
-                rootDirectory = mfMgr.getMediaFileDirectory(rootDirectory
-                        .getId());
-                testWeblog = TestUtils.getManagedWebsite(testWeblog);
-                MediaFile mediaFile = new MediaFile();
-                mediaFile.setName("test_file<index>.jpg".replace("<index>", i
-                        + ""));
-                mediaFile.setNotes("This is a test image");
-                mediaFile.setLength(2000);
-                mediaFile.setInputStream(getClass().getResourceAsStream(
-                        TEST_IMAGE));
-                mediaFile.setContentType("image/jpeg");
-                mediaFile.setDirectory(rootDirectory);
-                mfMgr.createMediaFile(testWeblog, mediaFile, new RollerMessages());
-                rootDirectory.getMediaFiles().add(mediaFile);
-                assertNotNull(mediaFile.getId());
-                assertNotNull(mediaFile.getId().length() > 0);
-                TestUtils.endSession(true);
-            }
-
-            testWeblog = TestUtils.getManagedWebsite(testWeblog);
-            MediaFileFilter filter1 = new MediaFileFilter();
-            filter1.setSize(1000);
-            filter1.setSizeFilterType(SizeFilterType.GT);
-            List<MediaFile> searchResults1 = mfMgr.searchMediaFiles(testWeblog,
-                    filter1);
-            assertFalse(searchResults1.isEmpty());
-            assertEquals(15, searchResults1.size());
-
-            MediaFileFilter filter2 = new MediaFileFilter();
-            filter2.setSize(1000);
-            filter2.setSizeFilterType(SizeFilterType.GT);
-            filter2.setStartIndex(5);
-            filter2.setLength(3);
-            List<MediaFile> searchResults2 = mfMgr.searchMediaFiles(testWeblog,
-                    filter2);
-            assertFalse(searchResults2.isEmpty());
-            assertEquals(3, searchResults2.size());
-            assertEquals("test_file13.jpg", searchResults2.get(0).getName());
-
-            MediaFileFilter filter3 = new MediaFileFilter();
-            filter3.setSize(1000);
-            filter3.setSizeFilterType(SizeFilterType.GT);
-            filter3.setStartIndex(13);
-            filter3.setLength(6);
-            List<MediaFile> searchResults3 = mfMgr.searchMediaFiles(testWeblog,
-                    filter3);
-            assertFalse(searchResults3.isEmpty());
-            assertEquals(2, searchResults3.size());
-            assertEquals("test_file8.jpg", searchResults3.get(0).getName());
-
-            MediaFileFilter filter4 = new MediaFileFilter();
-            filter4.setSize(1000);
-            filter4.setSizeFilterType(SizeFilterType.GT);
-            filter4.setStartIndex(14);
-            filter4.setLength(1);
-            List<MediaFile> searchResults4 = mfMgr.searchMediaFiles(testWeblog,
-                    filter4);
-            assertFalse(searchResults4.isEmpty());
-            assertEquals(1, searchResults4.size());
-            assertEquals("test_file9.jpg", searchResults4.get(0).getName());
-
-            TestUtils.endSession(true);
-        } finally {
-            TestUtils.teardownWeblog(testWeblog.getId());
-            TestUtils.teardownUser(testUser.getUserName());
-        }
-    }
-
-    /**
-     * Test searching media file with paging logic.
-     */
-    public void testSearchMediaFileOrderBy() throws Exception {
-        User testUser;
-        Weblog testWeblog;
-        testUser = TestUtils.setupUser("mediaFileTestUser10");
-        testWeblog = TestUtils.setupWeblog("mediaFileTestWeblog10", testUser);
-
-        MediaFileManager mfMgr = WebloggerFactory.getWeblogger()
-                .getMediaFileManager();
-
-        // no need to create root directory, that is done automatically now
-        MediaFileDirectory rootDirectory = mfMgr
-                .getDefaultMediaFileDirectory(testWeblog);
-
-        TestUtils.endSession(true);
-
-        testWeblog = TestUtils.getManagedWebsite(testWeblog);
-        rootDirectory = mfMgr.getMediaFileDirectory(rootDirectory.getId());
-        String[] contentTypes = { "image/gif", "image/jpeg", "image/bmp" };
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Timestamp(System.currentTimeMillis()));
-        for (int i = 0; i < 3; i++) {
-            MediaFile mediaFile = new MediaFile();
-            mediaFile
-                    .setName("test_file<index>.jpg".replace("<index>", i + ""));
-            mediaFile.setNotes("This is a test image");
-            mediaFile.setLength(2000);
-            mediaFile.setDirectory(rootDirectory);
-            mediaFile.setInputStream(getClass().getResourceAsStream(TEST_IMAGE));
-            mediaFile.setContentType(contentTypes[i]);
-
-            mediaFile.setDateUploaded(new Timestamp(cal.getTimeInMillis()));
-            // Add one second for date sql on mysql
-            cal.add(Calendar.SECOND, 1);
-            mfMgr.createMediaFile(testWeblog, mediaFile, new RollerMessages());
-            rootDirectory.getMediaFiles().add(mediaFile);
-            assertNotNull(mediaFile.getId());
-            assertNotNull(mediaFile.getId().length() > 0);
-        }
-        TestUtils.endSession(true);
-        testWeblog = TestUtils.getManagedWebsite(testWeblog);
-
-        MediaFileFilter filter1 = new MediaFileFilter();
-        filter1.setSize(1000);
-        filter1.setSizeFilterType(SizeFilterType.GT);
-        filter1.setOrder(MediaFileOrder.NAME);
-        List<MediaFile> searchResults1 = mfMgr.searchMediaFiles(testWeblog,
-                filter1);
-        assertFalse(searchResults1.isEmpty());
-        assertEquals(3, searchResults1.size());
-        assertEquals("test_file0.jpg", searchResults1.get(0).getName());
-        assertEquals("test_file1.jpg", searchResults1.get(1).getName());
-        assertEquals("test_file2.jpg", searchResults1.get(2).getName());
-
-        MediaFileFilter filter2 = new MediaFileFilter();
-        filter2.setSize(1000);
-        filter2.setSizeFilterType(SizeFilterType.GT);
-        filter2.setOrder(MediaFileOrder.TYPE);
-        List<MediaFile> searchResults2 = mfMgr.searchMediaFiles(testWeblog,
-                filter2);
-        assertFalse(searchResults2.isEmpty());
-        assertEquals(3, searchResults2.size());
-        assertEquals("test_file2.jpg", searchResults2.get(0).getName());
-        assertEquals("test_file0.jpg", searchResults2.get(1).getName());
-        assertEquals("test_file1.jpg", searchResults2.get(2).getName());
-
-        MediaFileFilter filter3 = new MediaFileFilter();
-        filter3.setSize(1000);
-        filter3.setSizeFilterType(SizeFilterType.GT);
-        filter3.setOrder(MediaFileOrder.DATE_UPLOADED);
-        List<MediaFile> searchResults3 = mfMgr.searchMediaFiles(testWeblog,
-                filter3);
-        assertFalse(searchResults3.isEmpty());
-        assertEquals(3, searchResults3.size());
-        assertEquals("test_file0.jpg", searchResults3.get(0).getName());
-        assertEquals("test_file1.jpg", searchResults3.get(1).getName());
-        assertEquals("test_file2.jpg", searchResults3.get(2).getName());
 
         TestUtils.endSession(true);
         TestUtils.teardownWeblog(testWeblog.getId());
