dd5181def6157f65e3137432cdf0833049d5624d
Glen Mazza <glen.mazza@gmail.com>
2015-11-02 22:55:00 -0500

    Issue 64: Remove unused strings from lang resource files.

--- docs/examples/i18n/compare-vs-default-file.py
+++ docs/examples/i18n/compare-vs-default-file.py
@@ -36,7 +36,7 @@ from contextlib import closing
 import sys
 
 def prop_names():
-    rfile = open("../../../app/src/main/resources/ApplicationResources_zh_CN.properties")
+    rfile = open("../../../app/src/main/resources/ApplicationResources_de.properties")
     prop_pattern = re.compile('^([a-zA-Z]+(\.[a-zA-Z]+)*)=.*')
     for line in rfile:
         m = prop_pattern.match(line)
