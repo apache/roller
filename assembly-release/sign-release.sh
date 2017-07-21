d80b7287f8fc6c1aec6b14264154bfc3b9d8fbb3
Dave Johnson <snoopdave@gmail.com>
2017-07-21 18:11:20 -0400

    Prepare for Roller 5..2.0 release.

--- assembly-release/sign-release.sh
+++ assembly-release/sign-release.sh
@@ -1,5 +1,15 @@
-export rcstring=""
+#!/usr/bin/env bash
+
+export rcstring="-rc-1"
 export vstring="5.2.0"
+
+# for rc releases we rename the release files
+if [ rcstring != "" ]; then
+    mv target/roller-release-${vstring}-source.tar.gz   target/roller-release-${vstring}${rcstring}-source.tar.gz
+    mv target/roller-release-${vstring}-source.zip      target/roller-release-${vstring}${rcstring}-source.zip
+    mv target/roller-release-${vstring}-standard.tar.gz target/roller-release-${vstring}${rcstring}-standard.tar.gz
+    mv target/roller-release-${vstring}-standard.zip    target/roller-release-${vstring}${rcstring}-standard.zip
+fi
 
 gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.tar.gz
 gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.zip
