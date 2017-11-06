2d19f5af74609f7f6aa75981b3887953a1b4055f
Dave Johnson <snoopdave@gmail.com>
2017-11-06 17:13:31 -0500

    Prepare for final release.

--- assembly-release/sign-release.sh
+++ assembly-release/sign-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export rcstring="-rc-6"
+export rcstring=""
 export vstring="5.2.0"
 
 # for rc releases we rename the release files
@@ -11,8 +11,8 @@ if [ rcstring != "" ]; then
     mv target/roller-release-${vstring}-standard.zip    target/roller-release-${vstring}${rcstring}-standard.zip
 fi
 
-gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.tar.gz \
-target/roller-release-${vstring}${rcstring}-standard.zip \
-target/roller-release-${vstring}${rcstring}-source.tar.gz \
-target/roller-release-${vstring}${rcstring}-source.zip
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.tar.gz 
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-standard.zip 
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-source.tar.gz 
+gpg --armor --detach-sig target/roller-release-${vstring}${rcstring}-source.zip
 
