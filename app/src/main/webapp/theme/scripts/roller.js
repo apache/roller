6a55bb734e4d539c990717c563594f79b341e502
Aditya Sharma <iamadityasharma7@gmail.com>
2020-05-11 19:45:14 +0530

    Implemented: validation using the jquery validation plugin (ROL-2156) Used validation on the Create weblog page

--- app/src/main/webapp/theme/scripts/roller.js
+++ app/src/main/webapp/theme/scripts/roller.js
@@ -210,3 +210,6 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+$(document).ready(function () {
+    jQuery("form.validate-form").validate();
+});
