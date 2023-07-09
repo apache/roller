b271cdd008b8ddb881af6012d9de4f907e6eee6b
David M. Johnson <snoopdave@apache.org>
2023-07-09 12:47:58 -0400

    Prepare for 6.1.2 release.

--- assembly-release/sign-release.sh
+++ assembly-release/sign-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export rcstring=""
-export vstring="6.1.2-SNAPSHOT"
+export rcstring="r1"
+export vstring="6.1.2"
 
 # for rc releases we rename the release files
 if [ rcstring != "" ]; then
