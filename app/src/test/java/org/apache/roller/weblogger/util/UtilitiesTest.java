aa03eaa13d3b89bf7d24d0ba8ff4e89566a0aa50
David M. Johnson <snoopdave@users.noreply.github.com>
2026-09-17 17:21:01 -0400

    Normalize links in HTML subset formatting (#190)

--- app/src/test/java/org/apache/roller/weblogger/util/UtilitiesTest.java
+++ app/src/test/java/org/apache/roller/weblogger/util/UtilitiesTest.java
@@ -19,13 +19,69 @@
 package org.apache.roller.weblogger.util;
 
 import org.junit.jupiter.api.Test;
+import org.apache.roller.weblogger.business.plugins.comment.HTMLSubsetPlugin;
+import org.apache.roller.weblogger.pojos.WeblogEntryComment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Test utilities.
  */
 public class UtilitiesTest  {
+
+    @Test
+    public void testHtmlSubsetLinkFormats() {
+        assertEquals("<a href=\"https://example.com/\">site</a>",
+                htmlSubset("<a href='https://example.com/'>site</a>"));
+        assertEquals("<a href=\"HTTP://example.com/\">site</a>",
+                htmlSubset("<A HREF=HTTP://example.com/>site</A>"));
+        assertEquals("<a href=\"mailto:reader@example.com\">mail</a>",
+                htmlSubset("<a href=\"mailto:reader@example.com\">mail</a>"));
+        assertEquals("<a href=\"https://example.com/?a=1&amp;b=2\">query</a>",
+                htmlSubset("<a href=\"https://example.com/?a=1&b=2\">query</a>"));
+        assertEquals("<a href=\"https://example.com/\">site</a>",
+                htmlSubset("<a\n href = 'https://example.com/' >site</a>"));
+        assertEquals("<a href=\"https://example.com/~reader\">site</a>",
+                htmlSubset("<a href=\"https://example.com/&#126;reader\">site</a>"));
+    }
+
+    @Test
+    public void testHtmlSubsetLinkSubset() {
+        assertEquals("<a>file</a>", htmlSubset("<a href=\"ftp://example.com/file\">file</a>"));
+        assertEquals("<a>local</a>", htmlSubset("<a href=\"../page\">local</a>"));
+        assertEquals("<a>site</a>",
+                htmlSubset("<a href=\"https://example.com/\" title=\"site\">site</a>"));
+        assertEquals("<a>empty</a>", htmlSubset("<a href=\"\">empty</a>"));
+    }
+
+    @Test
+    public void testHtmlSubsetRetainsTextAndFormatting() {
+        assertNull(Utilities.transformToHTMLSubset(null));
+        assertEquals("", htmlSubset(""));
+        String text = "<p><b>Bold</b> and <i>italic</i><br>line</p>";
+        assertEquals("<p><b>Bold</b> and <i>italic</i><br />line</p>", htmlSubset(text));
+        assertEquals("&#65; &lt;example&gt;", htmlSubset("&#65; &lt;example&gt;"));
+        assertEquals("&lt;a href=\"https://example.com/\"&gt;site&lt;/a&gt;",
+                htmlSubset("&lt;a href=\"https://example.com/\"&gt;site&lt;/a&gt;"));
+        assertEquals("<a href=\"https://example.com/$1\">one</a> <a>two</a>",
+                htmlSubset("<a href=\"https://example.com/$1\">one</a> <a href=\"ftp://example.com/\">two</a>"));
+    }
+
+    @Test
+    public void testHtmlSubsetPluginUsesLinkFormatting() {
+        WeblogEntryComment comment = new WeblogEntryComment();
+        comment.setContentType("text/html");
+        HTMLSubsetPlugin plugin = new HTMLSubsetPlugin();
+        String text = "<a href=\"ftp://example.com/\">file</a>";
+        assertEquals("<a>file</a>", plugin.render(comment, text));
+        comment.setContentType("text/plain");
+        assertEquals(text, plugin.render(comment, text));
+    }
+
+    private String htmlSubset(String text) {
+        return Utilities.transformToHTMLSubset(Utilities.escapeHTML(text));
+    }
 
     @Test
     public void testExtractHTML() {
